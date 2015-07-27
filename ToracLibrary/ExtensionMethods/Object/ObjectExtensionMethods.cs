6cc88bede4643df53c0c594cc8a1011814c64ecd
dibiancoj@gmail.com <dibiancoj@gmail.com>
2015-07-27 14:36:03 -0400

    object extension methods

---
+++ ToracLibrary/ExtensionMethods/Object/ObjectExtensionMethods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToracLibrary.ExtensionMethods.IEnumerableExtensions;
+
+namespace ToracLibrary.ExtensionMethods.ISetExtensions
+{
+
+    /// <summary>
+    /// Extension Methods For Objects
+    /// </summary>
+    public static class ObjectExtensionMethods
+    {
+
+        #region Single Object To List
+
+        /// <summary>
+        /// Pushes a single object to IEnumerable of that object type (T)
+        /// </summary>
+        /// <typeparam name="T">Type Of The Item Passed In</typeparam>
+        /// <param name="ItemToPutInArray">Item To Push Into The IEnumerable</param>
+        /// <returns>IEnumerable Of That Object Type, With The Item In The IEnumerable</returns>
+        public static IEnumerable<T> ToIEnumerable<T>(this T ItemToPutInArray)
+        {
+            //to call this method
+            //ClaimTeamToSave.ToIEnumerable();
+
+            //calling the ToIList To Share The Functionality And Not Duplicate Code
+            return ItemToPutInArray.ToIList();
+        }
+
+        /// <summary>
+        /// Pushes a single object to a list of that object type (T) and returns IList Of T
+        /// </summary>
+        /// <typeparam name="T">Type Of The Item Passed In</typeparam>
+        /// <param name="ItemToPutInArray">Item To Push Into The IEnumerable</param>
+        /// <returns>IList Of That Object Type, With The Item In The IList</returns>
+        public static IList<T> ToIList<T>(this T ItemToPutInArray)
+        {
+            //to call this method
+            //ClaimTeamToSave.ToIList();
+
+            //because it's an extension method of T...it's basically an object type because this will show up for every type when the namespace is imported
+
+            //return the new array with the item in it
+            return new List<T> { ItemToPutInArray };
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ ToracLibraryTest/ExtensionMethods/ObjectExtensions/ObjectExtensionTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToracLibrary.ExtensionMethods.ISetExtensions;
+using ToracLibraryTest.Framework.DummyObjects;
+
+namespace ToracLibraryTest.ExtensionMethods.ObjectTest
+{
+
+    /// <summary>
+    /// Unit test to Object Extension Methods
+    /// </summary>
+    [TestClass]
+    public class ObjectExtensionTest
+    {
+
+        /// <summary>
+        /// Unit test to create an IEnumerable from a single object
+        /// </summary>
+        [TestMethod]
+        public void SingleObjectToIEnumerableTest1()
+        {
+            //grab a single record and push to an ienumerable
+            var IEnumerableBuiltFromSingleObject = DummyObject.CreateDummyRecord().ToIEnumerable();
+
+            //make sure we only have 1 record. This should prove it's in a form of ienumerable
+            Assert.AreEqual(1, IEnumerableBuiltFromSingleObject.Count());
+        }
+
+        /// <summary>
+        /// Unit test to create an IList from a single object
+        /// </summary>
+        [TestMethod]
+        public void SingleObjectToListTest1()
+        {
+            //grab a single record and push to an ienumerable
+            var IListBuiltFromSingleObject = DummyObject.CreateDummyRecord().ToIList();
+
+            //make sure we only have 1 record. This should prove it's in a form of ienumerable
+            Assert.AreEqual(1, IListBuiltFromSingleObject.Count);
+
+            //add another record so we can make sure it increments
+            IListBuiltFromSingleObject.Add(DummyObject.CreateDummyRecord());
+
+            //check the count
+            Assert.AreEqual(2, IListBuiltFromSingleObject.Count);
+        }
+
+    }
+
+}
