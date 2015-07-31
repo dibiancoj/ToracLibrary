3d6938d069e91878980eafabc5a1bb205e4ec615
dibiancoj@gmail.com <dibiancoj@gmail.com>
2015-07-31 10:54:03 -0400

    regular expression

---
+++ ToracLibrary/RegularExpressions/HtmlTagRemoval.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.RegularExpressions
+{
+
+    /// <summary>
+    /// Removes Html Tags From A String.
+    /// </summary>
+    public static class HtmlTagRemoval
+    {
+
+        #region Public Static Variables
+
+        /// <summary>
+        /// Compiled Regular Expression To Grab Html Tags In A String. Compile Regex Is Used For Performance. Made Public To Allow Other Methods To Use It If You Want A Different Implementation
+        /// </summary>
+        private static readonly Regex HtmlCompiledRegExStatement = new Regex("<.*?>", RegexOptions.Compiled);
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Remove HTML From String With Compiled Regex. Will Replace The Html Tags With String.Empty. Use Overload If You Want To Replace It With A Custom Value You Pass In
+        /// </summary>
+        /// <param name="StringToStrip">String To Strip</param>
+        /// <returns>String Passed In Minus The Html Tags</returns>
+        public static string StripHtmlTags(string StringToStrip)
+        {
+            return HtmlCompiledRegExStatement.Replace(StringToStrip, string.Empty);
+        }
+
+        /// <summary>
+        /// Remove HTML From String With Compiled Regex And Replace It With The Replace Value You Passed In
+        /// </summary>
+        /// <param name="StringToStrip">String To Strip</param>
+        /// <param name="ReplaceValue">The Value You Want To Replace The Tags With</param>
+        /// <returns>String Passed In Minus The Html Tags</returns>
+        public static string StripHtmlTags(string StringToStrip, string ReplaceValue)
+        {
+            return HtmlCompiledRegExStatement.Replace(StringToStrip, ReplaceValue);
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ ToracLibrary/RegularExpressions/NumberParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.RegularExpressions
+{
+
+    /// <summary>
+    /// Take a string and return only the numbers in that string. ie 123jason945 will return 123945
+    /// </summary>
+    public static class NumberParser
+    {
+
+        #region Public Static Variables
+
+        /// <summary>
+        /// Compiled Regular Expression To Grab Only The Numbers In A String. Compile And Made Public So This Reg-Ex Statement Can Be Re-Used
+        /// </summary>
+        private static readonly Regex NumberOnlyCompiledRegExStatement = new Regex(@"[\d-]", RegexOptions.Compiled);
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Take a string and return only the numbers in that string. ie 123jason945 will return 123945
+        /// </summary>
+        /// <param name="StringToParse">String To Parse</param>
+        /// <returns>string with only numbers found in the original string</returns>
+        public static string ParseStringAndLeaveOnlyNumbers(string StringToParse)
+        {
+            return NumberOnlyCompiledRegExStatement.Replace(StringToParse, string.Empty);
+        }
+
+        /// <summary>
+        /// Take a string and replace only the text (any non numeric text) in that string. ie 123jason945 will return 123ReplaceValue945
+        /// </summary>
+        /// <param name="StringToParse">String To Parse</param>
+        /// <param name="ReplaceValue">Text You Want To Replace The Non Numeric Data Found</param>
+        /// <returns>string with only numbers found in the original string</returns>
+        public static string ParseStringAndLeaveOnlyNumbers(string StringToParse, string ReplaceValue)
+        {
+            return NumberOnlyCompiledRegExStatement.Replace(StringToParse, ReplaceValue);
+        }
+
+        #endregion
+
+    }
+
+}
