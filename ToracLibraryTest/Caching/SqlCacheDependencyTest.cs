d2100b551297d1ee54f4fdadb8664b443f43d996
dibiancoj@gmail.com <dibiancoj@gmail.com>
2015-07-31 16:22:26 -0400

    in memory unit tests

--- ToracLibraryTest/Caching/SqlCacheDependencyTest.cs
+++ ToracLibraryTest/Caching/SqlCacheDependencyTest.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using ToracLibrary.Core.DateTimeHelpers;
-using ToracLibrary.Core.DateTimeHelpers.BusinessHours;
 
 namespace ToracLibraryTest.UnitsTest.Caching
 {
