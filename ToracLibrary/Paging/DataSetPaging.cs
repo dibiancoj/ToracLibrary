569d2c459dac75b8406c185acf8ec0c5653f14e1
dibiancoj@gmail.com <dibiancoj@gmail.com>
2015-07-31 10:47:13 -0400

    paging

---
+++ ToracLibrary/Paging/DataSetPaging.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.Paging
+{
+
+    /// <summary>
+    /// Helps page data for a data set
+    /// </summary>
+    public static class DataSetPaging
+    {
+
+        /// <summary>
+        /// Calculates Total Number Of Pages In This Grid (Property Of Total Above - Need Additional Data To Calculate)
+        /// </summary>
+        /// <param name="HowManyTotalRecords">Total Number Of Records (Not Just This Page But In The Entire RecordSet)</param>
+        /// <param name="HowManyRecordsPerPage">How Many Records Per Page</param>
+        /// <returns>Number Of Pages</returns>
+        public static int CalculateTotalPages(int HowManyTotalRecords, int HowManyRecordsPerPage)
+        {
+            //calculate how many pages we have
+            double Conversion = ((double)HowManyTotalRecords / (double)HowManyRecordsPerPage);
+
+            //do we have an even amount
+            if ((Conversion % 1) == 0)
+            {
+                //we have an even amount
+                return Convert.ToInt32(Conversion);
+            }
+
+            //we have an uneven amount...so grab the floor then add 1
+            return Convert.ToInt32(Math.Floor(Conversion)) + 1;
+        }
+
+    }
+
+}
---
+++ ToracLibraryTest/Paging/PageTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToracLibrary.Paging;
+
+namespace ToracLibraryTest.UnitsTest
+{
+
+    /// <summary>
+    /// Unit test to page data sets
+    /// </summary>
+    [TestClass]
+    public class DataSetPagingTest
+    {
+
+        /// <summary>
+        /// Test dataset paging. How many pages for a given dataset
+        /// </summary>
+        [TestMethod]
+        public void CalculateTheNumberOfPagesTest1()
+        {
+            //[5 total records with 5 per page, should be 1 pages]
+            Assert.AreEqual(1, DataSetPaging.CalculateTotalPages(5, 5));
+
+            //[6 total records with 3 per page, should be 2 pages]
+            Assert.AreEqual(2, DataSetPaging.CalculateTotalPages(6, 3));
+
+            //[6 total records with 4 per page, should be 2 pages]
+            Assert.AreEqual(2, DataSetPaging.CalculateTotalPages(6, 4));
+
+            //[1 total records with 4 per page, should be 1 pages]
+            Assert.AreEqual(1, DataSetPaging.CalculateTotalPages(1, 4));
+        }
+
+    }
+
+}
