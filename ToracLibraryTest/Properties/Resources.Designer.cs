344c8aa433b375dd1824596f2a2420a2616717fb
dibiancoj <DiBiancoj@gmail.com>
2015-08-02 19:43:18 -0400

    unit test name cleanup and organization

--- ToracLibraryTest/Properties/Resources.Designer.cs
+++ ToracLibraryTest/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ToracLibraryTest.UnitsTest.Core.Properties {
+namespace ToracLibraryTest.UnitsTest.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ToracLibraryTest.UnitsTest.Core.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ToracLibraryTest.UnitsTest.Core.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ToracLibraryTest.UnitsTest.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
