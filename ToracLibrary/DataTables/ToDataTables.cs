fed05ab52334ab2877d5b8dceda055b5c22e31cf
dibiancoj <DiBiancoj@gmail.com>
2015-08-08 13:44:32 -0400

    ToDataTable

---
+++ ToracLibrary/DataTables/ToDataTables.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ToracLibrary.Core.DataTypes;
+using ToracLibrary.Core.ExtensionMethods.ObjectExtensions;
+using ToracLibrary.Core.ReflectionDynamic;
+
+namespace ToracLibrary.Core.DataTableHelpers
+{
+
+    /// <summary>
+    /// Builds a data table from an object or a list of objects
+    /// </summary>
+    public static class ToDataTables
+    {
+
+        #region Public Methods
+
+        /// <summary>
+        /// Builds a data table off of an object
+        /// </summary>
+        /// <typeparam name="T">Type of the object to build off of</typeparam>
+        /// <param name="ObjectToBuildDataTableOffOf">object to build</param>
+        /// <param name="TableName">Table Name. Property Off Of The Data Table</param>
+        /// <returns>Data Table</returns>
+        public static DataTable BuildDataTableFromObject<T>(T ObjectToBuildDataTableOffOf, string TableName) where T : class
+        {
+            //*** we need to have different method names because if we pass in a list<T> it will go into this overload. Because a list<T> is T.
+            // then we get blow ups. we need to manually pass in which method to use
+
+            //if this is a list...then blow it up and tell the user to use the other overload
+            if (ObjectToBuildDataTableOffOf is IEnumerable)
+            {
+                throw new ArgumentOutOfRangeException("ObjectToBuildDataTableOffOf", "Please Use The BuildDataTableFromObjectList Overload When Passing In T Of IEnumerable");
+            }
+
+            //use the helper method...create an array of this object and pass it in
+            return BuildDataTableFromListOfObjects(ObjectToBuildDataTableOffOf.ToIEnumerableLazy(), TableName);
+        }
+
+        /// <summary>
+        /// Builds a data table off of the list of objects passed in
+        /// </summary>
+        /// <typeparam name="T">Type of the object to build off of</typeparam>
+        /// <param name="ObjectsToBuildDataTableOffOf">List of objects to build</param>
+        /// <param name="TableName">Table Name. Property Off Of The Data Table</param>
+        /// <returns>Data table</returns>
+        public static DataTable BuildDataTableFromListOfObjects<T>(IEnumerable<T> ObjectsToBuildDataTableOffOf, string TableName) where T : class
+        {
+            //*** we need to have different method names because if we pass in a list<T> it will go into this overload. Because a list<T> is T.
+            // then we get blow ups. we need to manually pass in which method to use
+
+            //create the data table to return
+            var DataTableToBuild = new DataTable(TableName);
+
+            //grab the properies we care about
+            var PropertiesToBuild = PropertiesToBuildOffOf<T>().ToArray();
+
+            //instead of using reflection to get each of the object's value which is slower (even though we re-use property info which helps)
+            //the expression tree is faster...so we are going to store the expression into the dictionary 
+            var PropertyCachedGetters = new Dictionary<string, Func<T, object>>();
+
+            //let's loop through the properties to build up the column def's 
+            foreach (PropertyInfo PropertyToBuild in PropertiesToBuild)
+            {
+                //if its a nullable field then we need to 
+                if (PropertyHelpers.IsNullableOfT(PropertyToBuild))
+                {
+                    //its some sort of nullable<int>...data set doesnt support this, so we get the underlying data type and set it to nullable
+                    DataTableToBuild.Columns.Add(new DataColumn(PropertyToBuild.Name, PropertyToBuild.PropertyType.GetGenericArguments()[0]) { AllowDBNull = true });
+                }
+                else
+                {
+                    //regular column, just add it
+                    DataTableToBuild.Columns.Add(new DataColumn(PropertyToBuild.Name, PropertyToBuild.PropertyType));
+                }
+
+                //now compile the expression and add it to the dictionary
+                PropertyCachedGetters.Add(PropertyToBuild.Name, PropertyHelpers.GetPropertyOfObjectExpressionFunc<T, object>(PropertyToBuild.Name).Compile());
+            }
+
+            //now we need to go through each object and add the row
+            foreach (T thisObject in ObjectsToBuildDataTableOffOf)
+            {
+                //let's create the new data row
+                DataRow thisDataRow = DataTableToBuild.NewRow();
+
+                //let's loop through all the properties and set the column
+                foreach (PropertyInfo PropertyToSet in PropertiesToBuild)
+                {
+                    //grab the value and set it...if its null we set the value to Db Null
+                    thisDataRow[PropertyToSet.Name] = PropertyCachedGetters[PropertyToSet.Name](thisObject) ?? DBNull.Value;
+                }
+
+                //let's add the data row to the data table
+                DataTableToBuild.Rows.Add(thisDataRow);
+            }
+
+            //return the data table
+            return DataTableToBuild;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Build the properties to build the data table off of. Ignores collections because we can't model that in a data table
+        /// </summary>
+        /// <typeparam name="T">Type of the object to build</typeparam>
+        /// <returns>list of property info to build off of</returns>
+        private static IEnumerable<PropertyInfo> PropertiesToBuildOffOf<T>()
+        {
+            //grab just PrimitiveTypes we care about. (no collections or anything like that)
+            IEnumerable<Type> PrimativeTypesToBuild = PrimitiveTypes.PrimitiveTypesSelect();
+
+            //grab just PrimitiveTypes we care about. No collections or anything like that and return the result
+            return typeof(T).GetProperties().Where(x => PrimativeTypesToBuild.Contains(x.PropertyType));
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ ToracLibraryTest/Core/DataTables/DataTableTest.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToracLibrary.Core.DataTableHelpers;
+
+namespace ToracLibraryTest.UnitsTest.Core
+{
+
+    /// <summary>
+    /// Unit test for data table functionality
+    /// </summary>
+    [TestClass]
+    public class DataTableTest
+    {
+
+        #region To Data Table Test
+
+        #region Framework
+
+        private class DataTableTestClass
+        {
+
+            #region Properties
+
+            public int Id { get; set; }
+            public string Txt { get; set; }
+            public IEnumerable<DataTableTestClass> TestCollection { get; set; }
+
+            #endregion
+
+            #region Static Methods
+
+            public static IEnumerable<DataTableTestClass> BuildListOfTLazy()
+            {
+                //Go build the items to add to the table
+                yield return new DataTableTestClass { Id = 1, Txt = "1", TestCollection = new List<DataTableTestClass>() { new DataTableTestClass() } };
+                yield return new DataTableTestClass { Id = 2, Txt = "2", TestCollection = new List<DataTableTestClass>() { new DataTableTestClass() } };
+                yield return new DataTableTestClass { Id = 3, Txt = "3", TestCollection = new List<DataTableTestClass>() { new DataTableTestClass() } };
+            }
+
+            #endregion
+
+        }
+
+        #endregion
+
+        #region Test
+
+        /// <summary>
+        /// test the conversion from IEnumerable of T to a data table. This tests the single object method
+        /// </summary>
+        [TestCategory("Core.DataTableHelpers")]
+        [TestCategory("Core")]
+        [TestMethod]
+        public void ToDataTableTest1()
+        {
+            //go build 1 single item
+            var SingleObject = new DataTableTestClass
+            {
+                Id = 1,
+                Txt = "1",
+                TestCollection = new List<DataTableTestClass> { new DataTableTestClass() }
+            };
+
+            //give it a name
+            const string TableNameToUse = "TestTableName";
+
+            //go grab the result of the method
+            var DataTableResult = ToDataTables.BuildDataTableFromObject(SingleObject, TableNameToUse);
+
+            //check the table name
+            Assert.AreEqual(TableNameToUse, DataTableResult.TableName);
+
+            //check the count of how many rows we have
+            Assert.AreEqual(1, DataTableResult.Rows.Count);
+
+            //check the column count 
+            Assert.AreEqual(2, DataTableResult.Columns.Count);
+
+            //check the id field value
+            Assert.AreEqual(1, DataTableResult.Rows[0]["Id"]);
+
+            //check the txt field value
+            Assert.AreEqual("1", DataTableResult.Rows[0]["Txt"]);
+        }
+
+        /// <summary>
+        /// test the conversion from IEnumerable of T to a data table. This tests when you pass in a list of T
+        /// </summary>
+        [TestCategory("Core.DataTableHelpers")]
+        [TestCategory("Core")]
+        [TestMethod]
+        public void ToDataTableTest2()
+        {
+            //go get the list of T to add to the data table
+            var RowsToTest = DataTableTestClass.BuildListOfTLazy().ToArray();
+
+            //give it a name
+            const string TableNameToUse = "TestTableName";
+
+            //go grab the result of the method
+            var DataTableResult = ToDataTables.BuildDataTableFromListOfObjects(RowsToTest, TableNameToUse);
+
+            //check the table name
+            Assert.AreEqual(TableNameToUse, DataTableResult.TableName);
+
+            //check how many rows we have
+            Assert.AreEqual(RowsToTest.Count(), DataTableResult.Rows.Count);
+
+            //check the column count
+            Assert.AreEqual(2, DataTableResult.Columns.Count);
+
+            //check row 1
+            Assert.AreEqual(RowsToTest.First().Id, DataTableResult.Rows[0]["Id"]);
+            Assert.AreEqual(RowsToTest.First().Txt, DataTableResult.Rows[0]["Txt"]);
+
+            //check row 2
+            Assert.AreEqual(RowsToTest[1].Id, DataTableResult.Rows[1]["Id"]);
+            Assert.AreEqual(RowsToTest[1].Txt, DataTableResult.Rows[1]["Txt"]);
+
+            //check row 3
+            Assert.AreEqual(RowsToTest[2].Id, DataTableResult.Rows[2]["Id"]);
+            Assert.AreEqual(RowsToTest[2].Txt, DataTableResult.Rows[2]["Txt"]);
+        }
+
+        /// <summary>
+        /// make sure if you pass a list in to the single method name, it will raise an derror
+        /// </summary>
+
+        [TestCategory("Core.DataTableHelpers")]
+        [TestCategory("Core")]
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ToDataTableTest3()
+        {
+            //run the single T method with a list, it should raise an error
+            ToDataTables.BuildDataTableFromObject(DataTableTestClass.BuildListOfTLazy().ToArray(), "TestTableName");
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+
+}
