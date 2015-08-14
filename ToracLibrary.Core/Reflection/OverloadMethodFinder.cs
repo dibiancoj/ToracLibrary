bb5b9ae5cff16781657e9a1b83e46ec83018397b
dibiancoj@gmail.com <dibiancoj@gmail.com>
2015-08-14 10:09:15 -0400

    remove unused using. IEnumerableExtensions

--- ToracLibrary.Core/Reflection/OverloadMethodFinder.cs
+++ ToracLibrary.Core/Reflection/OverloadMethodFinder.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using ToracLibrary.Core.ExtensionMethods.IEnumerableExtensions;
 
 namespace ToracLibrary.Core.Reflection
 {
