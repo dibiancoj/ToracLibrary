d6e5547289373c6237fdbec591b05e404d7fa02e
dibiancoj@gmail.com <dibiancoj@gmail.com>
2015-08-20 15:55:48 -0400

    start of mvc

---
+++ ToracLibrary.AspNetMVC/HtmlHelpers/MvcHtmlHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ToracLibrary.AspNetMVC.HtmlHelpers
+{
+
+    /// <summary>
+    ///  Gives you the ability to create a custom attribute helper
+    /// </summary>
+    public static class MvcHtmlHelpers
+    {
+
+        /// <summary>
+        /// Checks the evaluation. If it's met it will return the Output To Display If Predicate Is Met. Otherwise will return null so we don't output the value
+        /// </summary>
+        /// <typeparam name="TModel">Model Type Of The View</typeparam>
+        /// <param name="Helper">Helper Extension Method Object</param>
+        /// <param name="OutputAttributePredicate">Value if we should output the attribute</param>
+        /// <param name="OutputToDisplayIfPredicateIsMet">Output to return if we meet the predicate. Will return null if the predicate is not met</param>
+        /// <returns>Null if predicate is not met. Otherwise will return OutputToDisplayIfPredicateIsMet</returns>
+        public static string CustomOutputAttribute<TModel>(this HtmlHelper<TModel> Helper, bool OutputAttributePredicate, string OutputToDisplayIfPredicateIsMet)
+        {
+            //call this in a razor view using (after importing the namespace to this class)
+            //@Html.CustomOutputAttribute(x.Model =="bla bla", "data-id=5");
+
+            //did we meet the predicate
+            if (OutputAttributePredicate)
+            {
+                //we met the predicate, return the display
+                return OutputToDisplayIfPredicateIsMet;
+            }
+
+            //we didn't meet the predicate, so return null
+            return null;
+        }
+
+    }
+
+}
---
+++ ToracLibraryTest/AspNetMvc/HtmlHelpers/HtmlHelperTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ToracLibrary.AspNetMVC.HtmlHelpers;
+
+namespace ToracLibraryTest.UnitsTest.AspNetMVC
+{
+
+    /// <summary>
+    /// Unit test for asp.net mvc html helpers
+    /// </summary>
+    [TestClass]
+    public class HtmlHelperTest
+    {
+
+        [TestCategory("AspNetMVC.HtmlHelpers")]
+        [TestCategory("AspNetMVC")]
+        [TestMethod]
+        public void CustomOutputAttributeTest1()
+        {
+            //test list
+            int[] ContainsTest = { 1, 2, 3, 4, 5 };
+
+            //positive result where we output the attribute
+            Assert.AreEqual("data-id=5", new HtmlHelper().CustomOutputAttribute<int>(x => ContainsTest.Any(3), "data-id=5"));
+        }
+
+    }
+
+}
