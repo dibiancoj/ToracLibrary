c72d24916e089cfc9dd9b6b7f52e913a6e859142
dibiancoj@gmail.com <dibiancoj@gmail.com>
2015-08-24 08:38:04 -0400

    jqgrid inline filter builder

--- ToracLibraryTest/AspNetMvc/JqGrid/JqGridTest.cs
+++ ToracLibraryTest/AspNetMvc/JqGrid/JqGridTest.cs
@@ -5,9 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ToracLibrary.AspNetMVC.JqGrid;
+using ToracLibrary.AspNetMVC.JqGrid.InlineFilters;
 using ToracLibrary.AspNetMVC.UnitTestMocking;
+using ToracLibrary.Core.DataProviders.EntityFrameworkDP;
 using ToracLibraryTest.Framework;
 using ToracLibraryTest.Framework.DummyObjects;
+using ToracLibraryTest.UnitsTest.Core.DataProviders;
+using ToracLibraryTest.UnitsTest.Core.DataProviders.EntityFrameworkDP;
+using ToracLibraryTest.UnitsTest.EntityFramework.DataContext;
 
 namespace ToracLibraryTest.UnitsTest.AspNetMVC
 {
@@ -52,10 +57,75 @@ namespace ToracLibraryTest.UnitsTest.AspNetMVC
         [TestCategory("AspNetMVC.JqGrid")]
         [TestCategory("AspNetMVC")]
         [TestMethod]
-        public void JqGridInlineFilterQueryBuilderTest1()
+        public void JqGridInlineFilterQueryBuilderAndStatementTest1()
         {
-            //test functionality
-            Assert.Fail();
+            DataProviderSetupTearDown.AddRows(true);
+
+            using (var DP = DIUnitTestContainer.DIContainer.Resolve<EntityFrameworkDP<EntityFrameworkEntityDP>>(EntityFrameworkTest.ReadonlyDataProviderName))
+            {
+                //id to use
+                int IdToQuery = 2;
+                string DescriptionToQuery = "1";
+
+                //build the base query
+                var SqlQuery = DP.Fetch<Ref_Test>(false).AsQueryable();
+
+                //filters to use
+                var FilterOnId = new JqGridInlineFilter { ColumnName = nameof(Ref_Test.Id), UserEnteredValue = IdToQuery.ToString() };
+                var FilterOnDescription = new JqGridInlineFilter { ColumnName = nameof(Ref_Test.Description), UserEnteredValue = DescriptionToQuery };
+
+                //let's go grab the filters to use
+                var FilterQuery = JqGridInlineFilterQueryBuilder.BuildInlineFilterQuery<Ref_Test>(new JqGridInlineFilters { Filters = new JqGridInlineFilter[] { FilterOnId, FilterOnDescription }, Operation = JqGridInlineFilters.JqGridOperationType.AND }, false);
+
+                //now combine the filter query with the base query
+                var QueryResults = SqlQuery.Where(FilterQuery).ToArray();
+
+                //now test it
+                Assert.AreEqual(1, QueryResults.Length);
+
+                //make sure we have the id = 2
+                Assert.AreEqual(IdToQuery, QueryResults.First().Id);
+
+                //make sure we have the description
+                Assert.AreEqual(DescriptionToQuery, QueryResults.First().Description);
+            }
+        }
+
+        [TestCategory("AspNetMVC.JqGrid")]
+        [TestCategory("AspNetMVC")]
+        [TestMethod]
+        public void JqGridInlineFilterQueryBuilderOrStatementTest1()
+        {
+            DataProviderSetupTearDown.AddRows(true);
+
+            using (var DP = DIUnitTestContainer.DIContainer.Resolve<EntityFrameworkDP<EntityFrameworkEntityDP>>(EntityFrameworkTest.ReadonlyDataProviderName))
+            {
+                //id to use
+                int IdToQuery = 2;
+                string DescriptionToQuery = "4";
+
+                //build the base query
+                var SqlQuery = DP.Fetch<Ref_Test>(false).AsQueryable();
+
+                //filters to use
+                var FilterOnId = new JqGridInlineFilter { ColumnName = nameof(Ref_Test.Id), UserEnteredValue = IdToQuery.ToString() };
+                var FilterOnDescription = new JqGridInlineFilter { ColumnName = nameof(Ref_Test.Description), UserEnteredValue = DescriptionToQuery };
+
+                //let's go grab the filters to use
+                var FilterQuery = JqGridInlineFilterQueryBuilder.BuildInlineFilterQuery<Ref_Test>(new JqGridInlineFilters { Filters = new JqGridInlineFilter[] { FilterOnId, FilterOnDescription }, Operation = JqGridInlineFilters.JqGridOperationType.OR }, false);
+
+                //now combine the filter query with the base query
+                var QueryResults = SqlQuery.Where(FilterQuery).OrderBy(x => x.Id).ToArray();
+
+                //now test it
+                Assert.AreEqual(2, QueryResults.Length);
+
+                //make sure we have the id = 2 for the first record
+                Assert.AreEqual(IdToQuery, QueryResults[0].Id);
+
+                //check the 2nd record
+                Assert.AreEqual(DescriptionToQuery, QueryResults[1].Description);
+            }
         }
 
         #endregion
