8eab335b095b5d2f87d3a5e0db0702c8b6f6d7a8
dibiancoj@gmail.com <dibiancoj@gmail.com>
2015-08-31 09:52:32 -0400

    dynamic run time.

---
+++ ToracLibrary.DynamicRunTime/DynamicRuntimeCompiler.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ToracLibrary.Core.ExtensionMethods.IEnumerableExtensions;
+
+namespace ToracLibrary.DynamicRunTime
+{
+
+    /// <summary>
+    /// API to allow you to compile code from a string at run time using Roslyn.
+    /// </summary>
+    public static class DynamicRuntimeCompiler
+    {
+
+        /// <summary>
+        /// Compile code from a string at run time
+        /// </summary>
+        /// <param name="CodeToCompile">Code to compile from a string</param>
+        /// <param name="TypesToReferenceLocationPath">Types to reference. ie: typeof(object).Assembly.Location</param>
+        /// <returns>Compile Time Results. </returns>
+        public static DynamicRuntimeResults CompileCodeAtRunTimeLazy(string CodeToCompile, params string[] TypesToReferenceLocationPath)
+        {
+            //let's parse this into a syntax tree
+            var SyntaxTree = CSharpSyntaxTree.ParseText(CodeToCompile);
+
+            //let's go build the metadata references
+            var MetaDataReferencesToUse = new List<MetadataReference>();
+
+            //loop through the parameters
+            if (TypesToReferenceLocationPath.AnyWithNullCheck())
+            {
+                //loop through the location paths
+                foreach (var ReferenceLocation in TypesToReferenceLocationPath)
+                {
+                    //add the reference
+                    MetaDataReferencesToUse.Add(MetadataReference.CreateFromFile(ReferenceLocation));
+                }
+            }
+
+            //go compile this thing
+            var Compilation = CSharpCompilation.Create(
+                Path.GetRandomFileName(),
+                syntaxTrees: new[] { SyntaxTree },
+                references: MetaDataReferencesToUse,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            //return errors
+            IEnumerable<Diagnostic> FailuresOfCompilation = null;
+
+            //the updated assembly
+            Assembly UpdatedAssembly = null;
+
+            //let's go write the result
+            using (var MemoryStreamToWrite = new MemoryStream())
+            {
+                //go emit the result
+                var ResultOfCompilation = Compilation.Emit(MemoryStreamToWrite);
+
+                //were we able to compile?
+                if (!ResultOfCompilation.Success)
+                {
+                    //do we have any failures
+                    FailuresOfCompilation = ResultOfCompilation.Diagnostics.Where(diagnostic => diagnostic.IsWarningAsError || diagnostic.Severity == DiagnosticSeverity.Error);
+                }
+                else
+                {
+                    //we were able to compile, start at the beg of the memory stream
+                    MemoryStreamToWrite.Seek(0, SeekOrigin.Begin);
+
+                    //now load the memory stream we wrote about
+                    UpdatedAssembly = Assembly.Load(MemoryStreamToWrite.ToArray());
+                }
+            }
+
+            //return the model now
+            return new DynamicRuntimeResults(FailuresOfCompilation, UpdatedAssembly);
+        }
+
+    }
+
+}
---
+++ ToracLibrary.DynamicRunTime/DynamicRuntimeResults.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ToracLibrary.Core.ExtensionMethods.IEnumerableExtensions;
+
+namespace ToracLibrary.DynamicRunTime
+{
+
+    /// <summary>
+    /// Holds the results of the dynamic run time compilation
+    /// </summary>
+    /// <remarks>Class is immutable</remarks>
+    public class DynamicRuntimeResults
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="CompileErrorsToSet">Compile Time Errors</param>
+        /// <param name="UpdatedAssemblyToSet">Updated Assembly</param>
+        public DynamicRuntimeResults(IEnumerable<Diagnostic> CompileErrorsToSet, Assembly UpdatedAssemblyToSet)
+        {
+            CompileErrors = CompileErrorsToSet;
+            UpdatedAssembly = UpdatedAssemblyToSet;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Compile Errors
+        /// </summary>
+        private IEnumerable<Diagnostic> CompileErrors { get; }
+
+        /// <summary>
+        /// Assembly that has the new code
+        /// </summary>
+        public Assembly UpdatedAssembly { get; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Gets the compile time errors
+        /// </summary>
+        /// <returns>yield return Diagnostic. Call CompileError.GetMessage() for full detail</returns>
+        public IEnumerable<Diagnostic> CompileTimeErrorsLazy()
+        {
+            //do we have any errors?
+            if (CompileErrors.AnyWithNullCheck())
+            {
+                //loop through the errors, and yield it
+                foreach (var Error in CompileErrors)
+                {
+                    yield return Error;
+                }
+            }
+        }
+
+        #endregion
+
+    }
+
+}
