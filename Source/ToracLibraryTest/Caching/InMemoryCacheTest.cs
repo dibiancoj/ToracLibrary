16eeeebbc509330abd0e876a62d93e9fb73c146d
dibiancoj <DiBiancoj@gmail.com>
2015-11-06 21:26:05 -0500

    add a different style syntax for caching. Inherit from in memory cache. this is more of a sample if you want to do it that way.

--- Source/ToracLibraryTest/Caching/InMemoryCacheTest.cs
+++ Source/ToracLibraryTest/Caching/InMemoryCacheTest.cs
@@ -55,6 +55,17 @@ namespace ToracLibraryTest.UnitsTest.Caching
         public static class DummyObjectCacheNoDI
         {
 
+            #region Constants
+
+            /// <summary>
+            /// cache key to use
+            /// </summary>
+            internal const string DummyObjectNoDiCacheKey = "DummyObjectCache";
+
+            #endregion
+
+            #region Static Methods
+
             /// <summary>
             /// Common method so we can have 1 method that creates the in memory cache for "DummyObjectCache"
             /// </summary>
@@ -90,6 +101,58 @@ namespace ToracLibraryTest.UnitsTest.Caching
                 return DataSourceForCache;
             }
 
+            #endregion
+
+        }
+
+        /// <summary>
+        /// More of an example of how to implement the cache with a different style then above
+        /// </summary>
+        public class DummyObjectCacheDifferentStyleSyntax : InMemoryCache<IEnumerable<DummyObject>>
+        {
+
+            #region Constructor
+
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            public DummyObjectCacheDifferentStyleSyntax()
+                : base(DifferentStyleSyntaxCacheKey, BuildCacheFromDataSource)
+            {
+            }
+
+            #endregion
+
+            #region Constants
+
+            /// <summary>
+            /// Key for this cache
+            /// </summary>
+            internal const string DifferentStyleSyntaxCacheKey = "DummyObjectCache2";
+
+            #endregion
+
+            #region Get From Data Source Implementation
+
+            /// <summary>
+            /// Grabs the data from the source and returns it
+            /// </summary>
+            /// <returns>list of dummy object</returns>
+            public static IEnumerable<DummyObject> BuildCacheFromDataSource()
+            {
+                //using a list because i don't want an iterator in the cache
+                var DataSourceForCache = new List<DummyObject>();
+
+                //add the cache items
+                DataSourceForCache.Add(new DummyObject(1, null));
+                DataSourceForCache.Add(new DummyObject(2, null));
+
+                //return the list now
+                return DataSourceForCache;
+            }
+
+            #endregion
+
         }
 
         #endregion
@@ -104,8 +167,11 @@ namespace ToracLibraryTest.UnitsTest.Caching
         [TestCategory("Caching")]
         public void InMemoryCacheTestWithNoDependencyInjectionTest1()
         {
+            //func to limit just the data in this cache
+            Func<KeyValuePair<string, object>, bool> OnlyThisCache = x => x.Key == DummyObjectCacheNoDI.DummyObjectNoDiCacheKey;
+
             //we will make sure nothing is in the cache
-            Assert.AreEqual(0, InMemoryCache.GetAllItemsInCacheLazy().Count());
+            Assert.AreEqual(0, InMemoryCache.GetAllItemsInCacheLazy().Count(OnlyThisCache));
 
             //grab the first item that we will test against. This should be the record "it should be"
             var RecordToCheckAgainst = DummyObjectCacheNoDI.BuildCacheDataSource().First();
@@ -114,22 +180,64 @@ namespace ToracLibraryTest.UnitsTest.Caching
             Assert.AreEqual(RecordToCheckAgainst.Id, DummyObjectCacheNoDI.GetCacheItem().ElementAt(0).Id);
 
             //just make sure we have that 1 item in the cache
-            Assert.AreEqual(1, InMemoryCache.GetAllItemsInCacheLazy().Count());
+            Assert.AreEqual(1, InMemoryCache.GetAllItemsInCacheLazy().Count(OnlyThisCache));
 
             //let's try to clear the cache now
             DummyObjectCacheNoDI.BuildCache().RemoveCacheItem();
 
             //make sure we have 0 records
-            Assert.AreEqual(0, InMemoryCache.GetAllItemsInCacheLazy().Count());
+            Assert.AreEqual(0, InMemoryCache.GetAllItemsInCacheLazy().Count(OnlyThisCache));
 
             //let's test the refresh now (we currently don't have an item in the cache, so it should handle if it's not there!)
             DummyObjectCacheNoDI.BuildCache().RefreshCacheItem();
 
             //that method should put the item back in... (1 element, because its only 1 cache we are using)
-            Assert.AreEqual(1, InMemoryCache.GetAllItemsInCacheLazy().Count());
+            Assert.AreEqual(1, InMemoryCache.GetAllItemsInCacheLazy().Count(OnlyThisCache));
 
             //let's just make sure we have 2 elements in the array
             Assert.AreEqual(DummyObjectCacheNoDI.BuildCacheDataSource().Count(), DummyObjectCacheNoDI.GetCacheItem().Count());
+        }
+
+        /// <summary>
+        /// Test the in memory cache using the different sytanx apporac
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Caching.InMemory")]
+        [TestCategory("Caching")]
+        public void InMemoryCacheTestWithDifferentSytnaxApproachTest1()
+        {
+            //let's grab an instance to the cache using the static method
+            var CacheBuilder = new DummyObjectCacheDifferentStyleSyntax();
+
+            //func to limit just the data in this cache
+            Func<KeyValuePair<string, object>, bool> OnlyThisCache = x => x.Key == DummyObjectCacheDifferentStyleSyntax.DifferentStyleSyntaxCacheKey;
+
+            //we will make sure nothing is in the cache
+            Assert.AreEqual(0, InMemoryCache.GetAllItemsInCacheLazy().Count(OnlyThisCache));
+
+            //grab the first item that we will test against. This should be the record "it should be"
+            var RecordToCheckAgainst = DummyObjectCacheDifferentStyleSyntax.BuildCacheFromDataSource().First();
+
+            //let's try to grab the first item...it should go get the item from the cache and return it
+            Assert.AreEqual(RecordToCheckAgainst.Id, CacheBuilder.GetCacheItem().ElementAt(0).Id);
+
+            //just make sure we have that 1 item in the cache
+            Assert.AreEqual(1, InMemoryCache.GetAllItemsInCacheLazy().Count(OnlyThisCache));
+
+            //let's try to clear the cache now
+            CacheBuilder.RemoveCacheItem();
+
+            //make sure we have 0 records
+            Assert.AreEqual(0, InMemoryCache.GetAllItemsInCacheLazy().Count(OnlyThisCache));
+
+            //let's test the refresh now (we currently don't have an item in the cache, so it should handle if it's not there!)
+            CacheBuilder.RefreshCacheItem();
+
+            //that method should put the item back in... (1 element, because its only 1 cache we are using)
+            Assert.AreEqual(1, InMemoryCache.GetAllItemsInCacheLazy().Count(OnlyThisCache));
+
+            //let's just make sure we have 2 elements in the array
+            Assert.AreEqual(DummyObjectCacheDifferentStyleSyntax.BuildCacheFromDataSource().Count(), CacheBuilder.GetCacheItem().Count());
         }
 
         /// <summary>
