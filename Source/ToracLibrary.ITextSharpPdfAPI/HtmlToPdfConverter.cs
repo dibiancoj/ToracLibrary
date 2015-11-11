274295a23ea678de399cd01fb06d1bb1bbdb4163
dibiancoj <dibiancoj@gmail.com>
2015-11-11 13:25:14 -0500

    html converter is a different class

---
+++ Source/ToracLibrary.ITextSharpPdfAPI/HtmlToPdfConverter.cs
@@ -0,0 +1,68 @@
+﻿using iTextSharp.tool.xml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.ITextSharpPdfAPI
+{
+
+    /// <summary>
+    /// Converts html into a a pdf using ITextSharp
+    /// </summary>
+    /// <remarks>This uses itextsharp.xmlworker package</remarks>
+    public static class HtmlToPdfConverter
+    {
+
+        #region Conversion - Html To Pdf
+
+        /// <summary>
+        /// Take's html and puts it into a pdf document. Use this if you don't need css support
+        /// </summary>
+        /// <param name="Html">html to render to a pdf</param>
+        /// <param name="Css">Css file (null if you dont have any css)</param>
+        /// <param name="CreatorToUse">Creator to use. This way the end developer can setup the margins and landscape</param>
+        /// <returns>CreatorToUse with everything written to it</returns>
+        public static PDFCreator HtmlToPdf(string Html, PDFCreator CreatorToUse)
+        {
+            //create the html stream
+            using (var HtmlStream = new StringReader(Html))
+            {
+                //Parse the HTML
+                XMLWorkerHelper.GetInstance().ParseXHtml(CreatorToUse.Writer, CreatorToUse.Doc, HtmlStream);
+
+                //go return the creator now
+                return CreatorToUse;
+            }
+        }
+
+        /// <summary>
+        /// Take's html and puts it into a pdf document. Use this if you need css support
+        /// </summary>
+        /// <param name="Html">html to render to a pdf</param>
+        /// <param name="Css">Css file (null if you dont have any css)</param>
+        /// <param name="CreatorToUse">Creator to use. This way the end developer can setup the margins and landscape</param>
+        /// <returns>CreatorToUse with everything written to it</returns>
+        public static PDFCreator HtmlToPdf(string Html, string Css, PDFCreator CreatorToUse)
+        {
+            //create the pdf creator
+            using (var CssStream = new MemoryStream(Encoding.UTF8.GetBytes(Css)))
+            {
+                //create the html stream
+                using (var HtmlStream = new MemoryStream(Encoding.UTF8.GetBytes(Html)))
+                {
+                    //Parse the HTML
+                    XMLWorkerHelper.GetInstance().ParseXHtml(CreatorToUse.Writer, CreatorToUse.Doc, HtmlStream, CssStream);
+
+                    //go return the creator now
+                    return CreatorToUse;
+                }
+            }
+        }
+
+        #endregion
+    }
+
+}
--- Source/ToracLibrary.ITextSharpPdfAPI/PDFCreator.cs
+++ Source/ToracLibrary.ITextSharpPdfAPI/PDFCreator.cs
@@ -102,7 +102,7 @@ namespace ToracLibrary.ITextSharpPdfAPI
         /// <summary>
         /// PDF Writer
         /// </summary>
-        protected PdfWriter Writer { get; }
+        public PdfWriter Writer { get; }
 
         #region Dispose Properties
 
@@ -278,56 +278,6 @@ namespace ToracLibrary.ITextSharpPdfAPI
         {
             //create this action and return it
             Writer.AddJavaScript(PdfAction.JavaScript("app.runtimeHighlight = true;", Writer));
-        }
-
-        #endregion
-
-        #region Conversion - Html To Pdf
-
-        /// <summary>
-        /// Take's html and puts it into a pdf document. Use this if you don't need css support
-        /// </summary>
-        /// <param name="Html">html to render to a pdf</param>
-        /// <param name="Css">Css file (null if you dont have any css)</param>
-        /// <param name="CreatorToUse">Creator to use. This way the end developer can setup the margins and landscape</param>
-        /// <returns>CreatorToUse with everything written to it</returns>
-        /// <remarks>This uses itextsharp.xmlworker package</remarks>
-        public static PDFCreator HtmlToPdf(string Html, PDFCreator CreatorToUse)
-        {
-            //create the html stream
-            using (var HtmlStream = new StringReader(Html))
-            {
-                //Parse the HTML
-                XMLWorkerHelper.GetInstance().ParseXHtml(CreatorToUse.Writer, CreatorToUse.Doc, HtmlStream);
-
-                //go return the creator now
-                return CreatorToUse;
-            }
-        }
-
-        /// <summary>
-        /// Take's html and puts it into a pdf document. Use this if you need css support
-        /// </summary>
-        /// <param name="Html">html to render to a pdf</param>
-        /// <param name="Css">Css file (null if you dont have any css)</param>
-        /// <param name="CreatorToUse">Creator to use. This way the end developer can setup the margins and landscape</param>
-        /// <returns>CreatorToUse with everything written to it</returns>
-        /// <remarks>This uses itextsharp.xmlworker package</remarks>
-        public static PDFCreator HtmlToPdf(string Html, string Css, PDFCreator CreatorToUse)
-        {
-            //create the pdf creator
-            using (var CssStream = new MemoryStream(Encoding.UTF8.GetBytes(Css)))
-            {
-                //create the html stream
-                using (var HtmlStream = new MemoryStream(Encoding.UTF8.GetBytes(Html)))
-                {
-                    //Parse the HTML
-                    XMLWorkerHelper.GetInstance().ParseXHtml(CreatorToUse.Writer, CreatorToUse.Doc, HtmlStream, CssStream);
-
-                    //go return the creator now
-                    return CreatorToUse;
-                }
-            }
         }
 
         #endregion
