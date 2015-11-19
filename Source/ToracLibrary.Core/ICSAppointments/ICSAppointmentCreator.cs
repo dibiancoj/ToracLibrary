c03e30f6f4d9913c06a6ac965bb98e4dd1f0c8d3
dibiancoj <dibiancoj@gmail.com>
2015-11-19 14:13:01 -0500

    ics creation

---
+++ Source/ToracLibrary.Core/ICSAppointments/ICSAppointmentCreator.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.Core.ICSAppointments
+{
+
+    /// <summary>
+    /// Creates and .ics file format which creates an event in outlook, google calendar, etc.
+    /// </summary>
+    public static class ICSAppointmentCreator
+    {
+
+        #region Constants
+
+        /// <summary>
+        /// Format to use when we just want a full day.
+        /// </summary>
+        /// <remarks>Only public for unit testing</remarks>
+        public const string FormatDate = "yyyyMMdd";
+
+        /// <summary>
+        /// Format to use when we just want a full day.
+        /// </summary>
+        /// <remarks>Only public for unit testing</remarks>
+        public const string FormatSpecificDateTime = "yyyyMMddTHHmmss";
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Creates an .ics file which creates an event in outlook, google calendar, etc.
+        /// </summary>
+        /// <param name="IsFullDateAppointment">Is this a full day appointment. IE: 9/10/2015 to 9/11/2015. Or will the date time include the hours and minutes</param>
+        /// <param name="StartDateTimeOfAppointment">Start date or time of appointment</param>
+        /// <param name="EndDateTimeOfAppointment">End date or time of appointment</param>
+        /// <param name="SummaryOfAppointment">Summary description of the appointment</param>
+        /// <param name="LocationOfAppointment">Location of the appointment</param>
+        /// <returns>A String. Either call  System.IO.File.WriteAllText("test.ics", result) to write it to disk. Or Encoding.ASCII.GetBytes(input) to get it into a byte array for download</returns>
+        public static string CreateICSAppointment(bool IsFullDayAppointment, DateTime StartDateTimeOfAppointment, DateTime EndDateTimeOfAppointment, string SummaryOfAppointment, string LocationOfAppointment)
+        {
+            /*Syntax should be something like this
+             *BEGIN:VCALENDAR
+             *VERSION:2.0
+             *PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+             *BEGIN:VEVENT
+             *DTSTART: UTC Time  ==> if this is a full day use DTSTART;VALUE=DATE:20150930
+             *DTEND: (Same as DTStart)
+             *SUMMARY: bla bla
+             *LOCATION: New York
+             *END:VEVENT
+             *END:VCALENDAR
+             */
+
+            //we will use a string builder to write everything
+            var ICSWriter = new StringBuilder();
+
+            //there are basically 4 fields tht we need to fill in...start date, end date, summary, and location.
+
+            //I'm going to build it up instead of doing a string format for no reason
+            ICSWriter.AppendLine("BEGIN:VCALENDAR");
+            ICSWriter.AppendLine("VERSION:2.0");
+            ICSWriter.AppendLine("PRODID:-//hacksw/handcal//NONSGML v1.0//EN");
+            ICSWriter.AppendLine("BEGIN:VEVENT");
+
+            //start date now
+            const string StartDateLabel = "DTSTART";
+            const string EndDateLabel = "DTEND";
+
+            //is this a full day?
+            if (IsFullDayAppointment)
+            {
+                //full date format
+                const string FullDateSeperator = ";";
+
+                //full date format
+                const string FullDateFormat = "VALUE=DATE:";
+
+                //We basically need: 
+                //"DTSTART;Value=Date:TheFormattedDateNow"
+                //"DTEND;Value=Date:TheFormattedDateNow"
+
+                //add the start full date appointment
+                ICSWriter.Append(StartDateLabel).Append(FullDateSeperator).Append(FullDateFormat).Append(GetFormattedDate(StartDateTimeOfAppointment.Date)).Append(Environment.NewLine);
+
+                //add the end date now
+                ICSWriter.Append(EndDateLabel).Append(FullDateSeperator).Append(FullDateFormat).Append(GetFormattedDate(EndDateTimeOfAppointment.Date)).Append(Environment.NewLine);
+            }
+            else
+            {
+                //We basically need: 
+                //"DTSTART:Date:TheFormattedDateNow"
+                //"DTEND:Date:TheFormattedDateNow"
+
+                //date time seperator
+                const string SpecificDateTimeSeperator = ":";
+
+                //add the start date time value in UTC
+                ICSWriter.Append(StartDateLabel).Append(SpecificDateTimeSeperator).Append(GetFormattedDateTime(StartDateTimeOfAppointment)).Append(Environment.NewLine);
+
+                //add the end date time value in UTC
+                ICSWriter.Append(EndDateLabel).Append(SpecificDateTimeSeperator).Append(GetFormattedDateTime(EndDateTimeOfAppointment)).Append(Environment.NewLine);
+            }
+
+            //add the summary
+            ICSWriter.Append("SUMMARY:").Append(SummaryOfAppointment).Append(Environment.NewLine);
+
+            //add the location
+            ICSWriter.Append("LOCATION:").Append(LocationOfAppointment).Append(Environment.NewLine);
+
+            //add the closing brackets
+            ICSWriter.AppendLine("END:VEVENT");
+
+            //add the end calendar
+            ICSWriter.AppendLine("END:VCALENDAR");
+
+            //let's go return the results
+            return ICSWriter.ToString();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// return the formatted date that the ics file expects
+        /// </summary>
+        /// <param name="DateToBuild">Date to build up</param>
+        /// <returns>the formatted time in a string</returns>
+        public static string GetFormattedDate(DateTime DateToBuild)
+        {
+            //add them all together now
+            return DateToBuild.ToString(FormatDate);
+        }
+
+        /// <summary>
+        /// Return the formatted time that an ics file expects
+        /// </summary>
+        /// <param name="DateToBuild">Date to build up</param>
+        /// <returns>the formatted time in a string</returns>
+        public static string GetFormattedDateTime(DateTime DateToBuild)
+        {
+            //just make sure if number is 1 digit, then we make it ie 09.
+
+            //return it in the format they want. Don't use z at the end because we want it local time
+            return DateToBuild.ToString(FormatSpecificDateTime);
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Source/ToracLibraryTest/Core/ICSAppointments/ICSAppointmentCreatorTest.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToracLibrary.Core.ICSAppointments;
+
+namespace ToracLibraryTest.UnitsTest.Core
+{
+
+    /// <summary>
+    /// Unit test for .ics appointment creation
+    /// </summary>
+    [TestClass]
+    public class ICSAppointmentCreatorTest
+    {
+
+        /*Syntax should be something like this
+            *BEGIN:VCALENDAR
+            *VERSION:2.0
+            *PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+            *BEGIN:VEVENT
+            *DTSTART: UTC Time  ==> if this is a full day use DTSTART;VALUE=DATE:20150930
+            *DTEND: (Same as DTStart)
+            *SUMMARY: bla bla
+            *LOCATION: New York
+            *END:VEVENT
+            *END:VCALENDAR
+            */
+
+        #region Unit Test Methods
+
+        /// <summary>
+        /// Test the creation of the appointment with full dates
+        /// </summary>
+        [TestCategory("Core.ICS")]
+        [TestCategory("Core")]
+        [TestMethod]
+        public void ICSCreationWithFullDatesTest1()
+        {
+            //start date
+            DateTime StartDate = new DateTime(2015, 3, 5);
+
+            //end date
+            DateTime EndDate = new DateTime(2015, 3, 10);
+
+            //summary text
+            const string SummaryText = "Test Summary";
+
+            //location text
+            const string LocationText = "Location Text";
+
+            //go run it and grab the results
+            var ICSCreatedFile = ICSAppointmentCreator.CreateICSAppointment(true, StartDate, EndDate, SummaryText, LocationText);
+
+            //split the lines
+            var SplitByLine = ICSCreatedFile.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            //check the standard fields
+            ICSStandardFormatTest(SplitByLine);
+
+            //check the values that actually change
+            Assert.AreEqual("DTSTART;VALUE=DATE:" + StartDate.ToString(ICSAppointmentCreator.FormatDate), SplitByLine[4]);
+            Assert.AreEqual("DTEND;VALUE=DATE:" + EndDate.ToString(ICSAppointmentCreator.FormatDate), SplitByLine[5]);
+            Assert.AreEqual("SUMMARY:" + SummaryText, SplitByLine[6]);
+            Assert.AreEqual("LOCATION:" + LocationText, SplitByLine[7]); 
+        }
+
+        /// <summary>
+        /// Test the creation of the appointment with specific date times
+        /// </summary>
+        [TestCategory("Core.ICS")]
+        [TestCategory("Core")]
+        [TestMethod]
+        public void ICSCreationWithSpecificDateTimesTest1()
+        {
+            //start date
+            DateTime StartDate = new DateTime(2015, 3, 5, 3, 55, 0);
+
+            //end date
+            DateTime EndDate = new DateTime(2015, 3, 10, 4, 58, 0);
+
+            //summary text
+            const string SummaryText = "Test Summary 123";
+
+            //location text
+            const string LocationText = "Location Text 123";
+
+            //go run it and grab the results
+            var ICSCreatedFile = ICSAppointmentCreator.CreateICSAppointment(false, StartDate, EndDate, SummaryText, LocationText);
+
+            //split the lines
+            var SplitByLine = ICSCreatedFile.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            //check the standard fields
+            ICSStandardFormatTest(SplitByLine);
+
+            //check the values now
+            Assert.AreEqual("DTSTART:" + StartDate.ToString(ICSAppointmentCreator.FormatSpecificDateTime), SplitByLine[4]);
+            Assert.AreEqual("DTEND:" + EndDate.ToString(ICSAppointmentCreator.FormatSpecificDateTime), SplitByLine[5]);
+            Assert.AreEqual("SUMMARY:" + SummaryText, SplitByLine[6]);
+            Assert.AreEqual("LOCATION:" + LocationText, SplitByLine[7]);
+        }
+
+        #endregion
+
+        #region Helper Unit Test Methods
+
+        /// <summary>
+        /// Will test the standard fields (excludes the date, summary, and location fields)
+        /// </summary>
+        /// <param name="ResultsSplitByLine">The results split by new line</param>
+        /// <remarks>Will assert and raise whatever it needs too</remarks>
+        private void ICSStandardFormatTest(IList<string> ResultsSplitByLine)
+        {
+            //make sure we have 10 items
+            Assert.AreEqual(10, ResultsSplitByLine.Count);
+
+            //top of the items
+            Assert.AreEqual("BEGIN:VCALENDAR", ResultsSplitByLine[0]);
+            Assert.AreEqual("VERSION:2.0", ResultsSplitByLine[1]);
+            Assert.AreEqual("PRODID:-//hacksw/handcal//NONSGML v1.0//EN", ResultsSplitByLine[2]);
+            Assert.AreEqual("BEGIN:VEVENT", ResultsSplitByLine[3]);
+
+            //bottom of the items
+            Assert.AreEqual("END:VEVENT", ResultsSplitByLine[8]);
+            Assert.AreEqual("END:VCALENDAR", ResultsSplitByLine[9]);
+        }
+
+        #endregion
+
+    }
+
+}
