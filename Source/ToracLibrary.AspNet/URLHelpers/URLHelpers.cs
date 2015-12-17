d9fce52169ca9333742f2957bbafb3d6cb1d7375
dibiancoj <dibiancoj@gmail.com>
2015-12-17 14:41:50 -0500

    url

---
+++ Source/ToracLibrary.AspNet/URLHelpers/URLHelpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.AspNet.URLHelperMethods
+{
+
+    /// <summary>
+    /// Helpers for anything dealing with url's
+    /// </summary>
+    public static class URLHelpers
+    {
+
+        /// <summary>
+        /// Appends the passed in query strings to the url passed in.
+        /// </summary>
+        /// <param name="QueryStringsToAppend">Query strings to add to the url</param>
+        /// <param name="UrlToModify">URL to add the query strings into</param>
+        /// <returns>updated url</returns>
+        public static string AppendQueryStringToUrl(IEnumerable<KeyValuePair<string, string>> QueryStringsToAppend, string UrlToModify)
+        {
+            //let's build the url. Throw it into a uri builder
+            var URIToBuild = new UriBuilder(UrlToModify);
+
+            //parse the query strings
+            var UrlQuery = System.Web.HttpUtility.ParseQueryString(URIToBuild.Query);
+
+            //loop through each query string to add
+            foreach (var QueryStringToAdd in QueryStringsToAppend)
+            {
+                //do we already have this query string in the url builder?
+                if (UrlQuery[QueryStringToAdd.Key] == null)
+                {
+                    //add the query string to the builder
+                    UrlQuery[QueryStringToAdd.Key] = QueryStringToAdd.Value;
+                }
+            }
+
+            //set the query
+            URIToBuild.Query = UrlQuery.ToString();
+
+            //return the url now
+            return URIToBuild.ToString();
+        }
+
+    }
+
+}
---
+++ Source/ToracLibraryTest/AspNet/AspNetTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ToracLibrary.AspNet.URLHelperMethods;
+
+namespace ToracLibraryTest.UnitsTest.AspNet.AspNet
+{
+
+    /// <summary>
+    /// Unit test for common asp.net methods
+    /// </summary>
+    [TestClass]
+    public class UrlHelpersTest
+    {
+
+        #region Unit Tests
+
+        [TestCategory("AspNet.URLHelpers")]
+        [TestCategory("AspNet")]
+        [TestMethod]
+        public void QueryStringAppendToUrlTest1()
+        {
+            Assert.AreEqual("http://www.test.com:80/Index?Id=1", URLHelpers.AppendQueryStringToUrl(new KeyValuePair<string, string>[] { new KeyValuePair<string, string>("Id", "1") }, "http://www.test.com/Index"));
+            Assert.AreEqual("http://www.test.com:80/Index?Id=1&Id2=5", URLHelpers.AppendQueryStringToUrl(new KeyValuePair<string, string>[] { new KeyValuePair<string, string>("Id", "1"), new KeyValuePair<string, string>("Id2", "5") }, "http://www.test.com/Index"));
+            Assert.AreEqual("http://www.test.com:80/Index?Id=1", URLHelpers.AppendQueryStringToUrl(new KeyValuePair<string, string>[] { new KeyValuePair<string, string>("Id", "1"), new KeyValuePair<string, string>("Id", "5") }, "http://www.test.com/Index"));
+        }
+
+        #endregion
+
+    }
+
+}
