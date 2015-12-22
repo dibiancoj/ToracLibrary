ef215d61336b3f40dfce220c9ba00f010ed4c91b
Jason <dibiancoj@gmail.com>
2015-12-21 20:52:15 -0500

    t4 template test db script

--- Source/ToracLibraryTest/Framework/SqlCreateScripts/CreateDatabaseScripts.sql
+++ Source/ToracLibraryTest/Framework/SqlCreateScripts/CreateDatabaseScripts.sql
@@ -83,3 +83,30 @@ CREATE TABLE [dbo].[Ref_Test](
 GO
 SET ANSI_PADDING OFF
 GO
+
+GO
+/****** Object:  Table [dbo].[T4TemplateTest]    Script Date: 12/21/2015 8:51:47 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[T4TemplateTest](
+	[Id] [int] NOT NULL,
+	[Description] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_T4TemplateTest] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+INSERT [dbo].[T4TemplateTest] ([Id], [Description]) VALUES (1, N'Item1')
+GO
+INSERT [dbo].[T4TemplateTest] ([Id], [Description]) VALUES (2, N'Item2')
+GO
+INSERT [dbo].[T4TemplateTest] ([Id], [Description]) VALUES (3, N'Item3')
+GO
