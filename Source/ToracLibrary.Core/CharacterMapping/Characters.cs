a26c7bd4e0419d871decfc0c3a4671dc311c0f4e
dibiancoj <dibiancoj@gmail.com>
2016-01-22 17:08:24 -0500

    alphabet and characters

---
+++ Source/ToracLibrary.Core/CharacterMapping/Characters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.Core.CharacterMapping
+{
+
+    /// <summary>
+    /// Holds static list for alphabet and numeric characters
+    /// </summary>
+    public static class Characters
+    {
+
+        /// <summary>
+        /// Returns the number characters in the english language
+        /// </summary>
+        /// <returns>all the numbers in the english language in an iterator</returns>
+        public static IEnumerable<int> AllNumberCharactersLazy()
+        {
+            //loop through the numbers and yield them
+            for (int i = 0; i < 10; i++)
+            {
+                //return i
+                yield return i;
+            }
+        }
+
+        /// <summary>
+        /// Returns the alphabet characters in the english language
+        /// </summary>
+        /// <returns>all the alphabet characters in the english language in an iterator</returns>
+        /// <remarks>Call .ToUpper() if you want uppercase</remarks>
+        public static IEnumerable<char> AllAlphaBetCharactersLazy()
+        {
+            //loop through each character
+            foreach (var Character in "abcdefghijklmnopqrstuvwxyz")
+            {
+                //return the character
+                yield return Character;
+            }
+        }
+
+    }
+
+}
---
+++ Source/ToracLibraryTest/Core/CharacterMapping/CharacterMappingTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static ToracLibrary.Core.CharacterMapping.Characters;
+
+namespace ToracLibraryTest.UnitsTest.Core
+{
+
+    /// <summary>
+    /// Unit test to test the character mapping functionality
+    /// </summary>
+    [TestClass]
+    public class CharacterMappingTest
+    {
+
+        /// <summary>
+        /// Make sure the numeric numbers return are correct
+        /// </summary>
+        [TestCategory("Core.CharacterMapping")]
+        [TestCategory("Core")]
+        [TestMethod]
+        public void NumericCharactersTest1()
+        {
+            //going to hard code this test
+            var DigitsToTest = AllNumberCharactersLazy().OrderBy(x => x).ToArray();
+
+            //start testing this. going to do this manually to ensure everything is correct
+            for (int i = 0; i < 10; i++)
+            {
+                //test the result
+                Assert.AreEqual(i, DigitsToTest[i]);
+            }
+
+        }
+
+        /// <summary>
+        /// Make sure the alphabet numbers return are correct
+        /// </summary>
+        [TestCategory("Core.CharacterMapping")]
+        [TestCategory("Core")]
+        [TestMethod]
+        public void AlphabetCharactersTest1()
+        {
+            //loop through all the characters and test it
+            var ResultOfCall = AllAlphaBetCharactersLazy().ToArray();
+
+            //characters to test
+            var CharactersInAlphaBet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            //test all the characters now
+            foreach(var RequiredCharacter in CharactersInAlphaBet)
+            {
+                //make sure its there
+                Assert.IsTrue(ResultOfCall.Any(y => y == RequiredCharacter));
+            }
+        }
+
+    }
+
+}
