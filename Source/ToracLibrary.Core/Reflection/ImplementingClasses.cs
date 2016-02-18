c061fe0948d0e6a2bc33190ea67955c4ab1c37fd
Jason <dibiancoj@gmail.com>
2016-02-17 21:56:06 -0500

    code cleanup

--- Source/ToracLibrary.Core/Reflection/ImplementingClasses.cs
+++ Source/ToracLibrary.Core/Reflection/ImplementingClasses.cs
@@ -10,8 +10,22 @@ namespace ToracLibrary.Core.ReflectionDynamic
     /// <summary>
     /// Retrieve the classes that implement an interface
     /// </summary>
-    public class ImplementingClasses
+    public static class ImplementingClasses
     {
+
+        #region Private Static Methods
+
+        /// <summary>
+        /// Holds the function that tests that the type passed in inherits from the TestThatItInherits From
+        /// </summary>
+        private static readonly Func<Type, Type, bool> TypeInheritsFromClass = (TypeToCheck, TestThatItInheritsFromThisClass) => TypeToCheck.IsSubclassOf(TestThatItInheritsFromThisClass);
+
+        /// <summary>
+        /// Holds the function that tests that the type to check implements the interface to test.
+        /// </summary>
+        private static readonly Func<Type, Type, bool> TypeImplementsInterface = (TypeToCheck, TestInterfaceItMustImplement) => TestInterfaceItMustImplement.IsAssignableFrom(TypeToCheck);
+
+        #endregion
 
         #region Public Methods
 
@@ -33,23 +47,17 @@ namespace ToracLibrary.Core.ReflectionDynamic
         /// <returns>list of types (classes) that implement or derived from InterfaceOrBaseClass. You can call Activator.CreateInstance(thisType) to create an instance of the type passed back.</returns>
         public static IEnumerable<Type> RetrieveImplementingClassesLazy(Type InterfaceOrBaseClass)
         {
+            //are we checking for an interface or a base class
+            var MatchesCriteriaFunc = InterfaceOrBaseClass.IsInterface ? TypeImplementsInterface : TypeInheritsFromClass;
+
             //let loop through all the assemblies
             foreach (var AssemblyToCheck in AppDomain.CurrentDomain.GetAssemblies())
             {
                 //now loop through all the types in this assembly (ignore interfaces)
-                foreach (Type TypeInAssemblyToCheck in AssemblyToCheck.GetTypes().Where(x => !x.IsInterface))
+                foreach (Type TypeInAssemblyToCheck in AssemblyToCheck.GetTypes().Where(x => !x.IsInterface && MatchesCriteriaFunc(x, InterfaceOrBaseClass)))
                 {
-                    //if we are checking for interfaces then see if it's assignable from
-                    if (InterfaceOrBaseClass.IsInterface && InterfaceOrBaseClass.IsAssignableFrom(TypeInAssemblyToCheck))
-                    {
-                        //we are testing for interfaces (we have a match)
-                        yield return TypeInAssemblyToCheck;
-                    }
-                    else if (TypeInAssemblyToCheck.IsSubclassOf(InterfaceOrBaseClass))
-                    {
-                        //we are testing for base classes (we have a match)
-                        yield return TypeInAssemblyToCheck;
-                    }
+                    //we have a match. This either implements the interface or inherits from the base class
+                    yield return TypeInAssemblyToCheck;
                 }
             }
         }
