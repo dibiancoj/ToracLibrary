4addebb8746c9979ae7390093d57287dbde4d7ac
dibiancoj <dibiancoj@gmail.com>
2016-03-16 15:47:05 -0400

    remove method in cache. use the untyped method

--- Source/ToracLibrary.Caching/BaseClass/InMemoryCache.cs
+++ Source/ToracLibrary.Caching/BaseClass/InMemoryCache.cs
@@ -187,24 +187,6 @@ namespace ToracLibrary.Caching
 
         #endregion
 
-        #region Helper Methods
-
-        /// <summary>
-        /// Gets all the keys and their object that are in the cache at the present time 
-        /// </summary>
-        /// <returns>list of keys and the item in the cache. Lazy loads the return list using yield result</returns>
-        public static IEnumerable<KeyValuePair<string, object>> GetAllItemsInCacheLazy()
-        {
-            //* we have it in this method and the untyped class so a person can call this method without having to create something with T.
-            //this way an admin screen can get all the entries without having to create an entire implementation. They also have the luxury if they have 
-            //an implementation to get it that way too
-
-            //use the untyped overload
-            return GetAllItemsInCacheLazy();
-        }
-
-        #endregion
-
     }
 
 }
