557ce63277f657b3deaabd6b3465f844b1e104fc
dibiancoj <dibiancoj@gmail.com>
2016-04-01 10:39:48 -0400

    code comment

--- Source/ToracLibrary.Core/ExtensionMethods/StringsExtensions/StringExtensionMethods.cs
+++ Source/ToracLibrary.Core/ExtensionMethods/StringsExtensions/StringExtensionMethods.cs
@@ -169,6 +169,11 @@ namespace ToracLibrary.Core.ExtensionMethods.StringExtensions
         /// <returns>Boolean | True = Is Valid Email Format, False = It Is Not</returns>
         public static bool IsValidEmailAddress(this string EmailAddressToValidate)
         {
+            //the if statement version is much faster then mvc's [EmailAddress]. They all pass the same tests and it's simpler. (I profiled the 2 versions in benchmark dot net. Mine was faster by 30%)
+
+            //this is the mvc regex version incase we ever want to use it. For now i'm sticking with what i have
+            //var result = new Regex(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$", RegexOptions.Compiled).Match(email to test);
+
             //needs to be not null
             if (string.IsNullOrEmpty(EmailAddressToValidate))
             {
