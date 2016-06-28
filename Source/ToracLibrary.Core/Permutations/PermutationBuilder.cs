009dae1ae3af8ed59629ac2d0bb1f3cfac713bc4
dibiancoj <dibiancoj@gmail.com>
2016-06-28 15:48:19 -0400

    remove unused ref.

--- Source/ToracLibrary.Core/Permutations/PermutationBuilder.cs
+++ Source/ToracLibrary.Core/Permutations/PermutationBuilder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static ToracLibrary.Core.ExtensionMethods.IEnumerableExtensions.IEnumerableExtensionMethods;
 
 namespace ToracLibrary.Core.Permutations
 {
