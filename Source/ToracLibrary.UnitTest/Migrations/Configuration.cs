9166e15ff7acaea6e99dd2e92c241084813f3132
Jason <dibiancoj@gmail.com>
2016-08-27 00:09:31 -0400

    migration stuff

--- Source/ToracLibrary.UnitTest/Migrations/Configuration.cs
+++ Source/ToracLibrary.UnitTest/Migrations/Configuration.cs
@@ -1,18 +1,23 @@
 namespace ToracLibrary.UnitTest.Migrations
 {
+    using EntityFramework.DataContext;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ToracLibrary.UnitTest.EntityFramework.DataContext.EntityFrameworkEntityDP>
+    internal sealed class Configuration : DbMigrationsConfiguration<EntityFrameworkEntityDP>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+
+            //if you don't want the code to automatically update the database then uncomment this.
+            //if you uncomment this then you will have to do the update-database to update the database - instead of having the code automatically do this
+            //Database.SetInitializer<EntityFrameworkEntityDP>(null);
         }
 
-        protected override void Seed(ToracLibrary.UnitTest.EntityFramework.DataContext.EntityFrameworkEntityDP context)
+        protected override void Seed(EntityFrameworkEntityDP context)
         {
             //  This method will be called after migrating to the latest version.
 
