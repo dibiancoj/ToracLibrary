1acc324f869f3cd6f98ea536ab8cd487157cf21d
Jason <dibiancoj@gmail.com>
2016-10-06 21:04:04 -0400

    remove the plus minus parser code. will leave in solution for example

--- Source/ToracLibrary.Parser/Parser/PlusMinusParser.cs
+++ Source/ToracLibrary.Parser/Parser/PlusMinusParser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,123 +12,128 @@ using ToracLibrary.Parser.Tokenizer.Tokens.OperatorTokens;
 namespace ToracLibrary.Parser.Parser
 {
 
-    /// <summary>
-    /// Takes the tokens and parses the values
-    /// </summary>
-    public static class PlusMinusParser
-    {
 
-        #region Public Methods
+ /// <summary>
+ /// Takes the tokens and parses the values
+ /// </summary>
+ public static class PlusMinusParser
+ {
+     ********started out using this for the plus - minus parser. To add * and / i had to change the math formula to calculate (orders of operation). This got left behind. leaving this in here for another example of how to loop through and parse ********
 
-        /// <summary>
-        /// Parse the tokens and return the result of the expression
-        /// </summary>
-        /// <param name="Tokens">Tokens found in expression</param>
-        /// <param name="ValidTokens">Supported Tokens for this expression type</param>
-        /// <returns>the calculated value</returns>
-        public static double Parse(IEnumerable<TokenBase> Tokens, ISet<ITokenFactory> ValidTokens)
-        {
-            //use an enumerator
-            using (var Token = Tokens.GetEnumerator())
-            {
-                //holds the result of the expression
-                double ResultOfExpression = 0;
+     #region Public Methods
 
-                //loop through all the tokens
-                while (Token.MoveNext())
-                {
-                    //parse this value
-                    ResultOfExpression = ParseEquationExpression(Token);
+     /// <summary>
+     /// Parse the tokens and return the result of the expression
+     /// </summary>
+     /// <param name="Tokens">Tokens found in expression</param>
+     /// <param name="ValidTokens">Supported Tokens for this expression type</param>
+     /// <returns>the calculated value</returns>
+     public static double Parse(IEnumerable<TokenBase> Tokens, ISet<ITokenFactory> ValidTokens)
+     {
+         //use an enumerator
+         using (var Token = Tokens.GetEnumerator())
+         {
+             //holds the result of the expression
+             double ResultOfExpression = 0;
 
-                    //can we move next?
-                    if (Token.MoveNext())
-                    {
-                        //is this an operator token?
-                        if (Token.Current is OperatorBaseToken)
-                        {
-                            //grab the current token
-                            var OperationToExecute = Token.Current;
+             //loop through all the tokens
+             while (Token.MoveNext())
+             {
+                 //parse this value
+                 ResultOfExpression = ParseEquationExpression(Token);
 
-                            //we have the operator token...move to the next item now so we can grab the value to calcualte
-                            Token.MoveNext();
+                 //can we move next?
+                 if (Token.MoveNext())
+                 {
+                     //is this an operator token?
+                     if (Token.Current is OperatorBaseToken)
+                     {
+                         //grab the current token
+                         var OperationToExecute = Token.Current;
 
-                            //parse the second number
-                            var SecondNumber = ParseEquationExpression(Token);
+                         //we have the operator token...move to the next item now so we can grab the value to calcualte
+                         Token.MoveNext();
 
-                            //go calculate the operation and return the number
-                            return ((OperatorBaseToken)OperationToExecute).Calculate(ResultOfExpression, SecondNumber);
-                        }
+                         //parse the second number
+                         var SecondNumber = ParseEquationExpression(Token);
 
-                        //what are we trying to do with this token?
-                        throw new ExpectingTokenException(typeof(OperatorBaseToken), Token.Current);
-                    }
-                }
+                         //go calculate the operation and return the number
+                         return ((OperatorBaseToken)OperationToExecute).Calculate(ResultOfExpression, SecondNumber);
+                     }
 
-                //return the result
-                return ResultOfExpression;
-            }
-        }
+                     //what are we trying to do with this token?
+                     throw new ExpectingTokenException(typeof(OperatorBaseToken), Token.Current);
+                 }
+             }
 
-        #endregion
+             //return the result
+             return ResultOfExpression;
+         }
+     }
 
-        #region Private Methods
+     #endregion
 
-        /// <summary>
-        /// Parses a specific equation. {{Number}} {{Operator}} {{Number}}
-        /// </summary>
-        /// <param name="TokenEnumerator">Token enumerator with the continue enumerator from base method</param>
-        /// <returns>The result of the equation</returns>
-        private static double ParseEquationExpression(IEnumerator<TokenBase> TokenEnumerator)
-        {
-            //this is the start of the equation. This should be the start of the literal
-            var NumberLiteral = ParseNumber(TokenEnumerator.Current);
+     #region Private Methods
 
-            //if we have no more items..then just return the number
-            if (!TokenEnumerator.MoveNext())
-            {
-                return NumberLiteral;
-            }
+     /// <summary>
+     /// Parses a specific equation. {{Number}} {{Operator}} {{Number}}
+     /// </summary>
+     /// <param name="TokenEnumerator">Token enumerator with the continue enumerator from base method</param>
+     /// <returns>The result of the equation</returns>
+     private static double ParseEquationExpression(IEnumerator<TokenBase> TokenEnumerator)
+     {
+         //this is the start of the equation. This should be the start of the literal
+         var NumberLiteral = ParseNumber(TokenEnumerator.Current);
 
-            //if we need to some operations
-            if (TokenEnumerator.Current is OperatorBaseToken)
-            {
-                //which operation should we execute?
-                var OperationToExecute = TokenEnumerator.Current;
+         //if we have no more items..then just return the number
+         if (!TokenEnumerator.MoveNext())
+         {
+             return NumberLiteral;
+         }
 
-                //move to the next item in the reader
-                TokenEnumerator.MoveNext();
+         //if we need to some operations
+         if (TokenEnumerator.Current is OperatorBaseToken)
+         {
+             //which operation should we execute?
+             var OperationToExecute = TokenEnumerator.Current;
 
-                //this should be the 2nd number now
-                var SecondNumberInExpression = ParseNumber(TokenEnumerator.Current);
+             //move to the next item in the reader
+             TokenEnumerator.MoveNext();
 
-                //go calculate the operation and return the number
-                return ((OperatorBaseToken)OperationToExecute).Calculate(NumberLiteral, SecondNumberInExpression);
-            }
+             //this should be the 2nd number now
+             var SecondNumberInExpression = ParseNumber(TokenEnumerator.Current);
 
-            //it should have a number here
-            throw new Exception("Expecting operator after number, but got " + TokenEnumerator.Current);
-        }
+             //go calculate the operation and return the number
+             return ((OperatorBaseToken)OperationToExecute).Calculate(NumberLiteral, SecondNumberInExpression);
+         }
 
-        /// <summary>
-        /// Parse the number for the given token
-        /// </summary>
-        /// <param name="TokenToParse">Token to parse</param>
-        /// <returns>The value of the token constant</returns>
-        private static double ParseNumber(TokenBase TokenToParse)
-        {
-            //is this a number constant token?
-            if (TokenToParse is NumberLiteralToken)
-            {
-                //it is...return the value
-                return ((NumberLiteralToken)TokenToParse).Value;
-            }
+         //it should have a number here
+         throw new Exception("Expecting operator after number, but got " + TokenEnumerator.Current);
+     }
 
-            //we should have a literal..so throw an error
-            throw new Exception("Expected a number constant but found " + TokenToParse);
-        }
+     /// <summary>
+     /// Parse the number for the given token
+     /// </summary>
+     /// <param name="TokenToParse">Token to parse</param>
+     /// <returns>The value of the token constant</returns>
+     private static double ParseNumber(TokenBase TokenToParse)
+     {
+         //is this a number constant token?
+         if (TokenToParse is NumberLiteralToken)
+         {
+             //it is...return the value
+             return ((NumberLiteralToken)TokenToParse).Value;
+         }
 
-        #endregion
+         //we should have a literal..so throw an error
+         throw new Exception("Expected a number constant but found " + TokenToParse);
+     }
 
-    }
+     #endregion
 
 }
+
+}
+
+
+  */
