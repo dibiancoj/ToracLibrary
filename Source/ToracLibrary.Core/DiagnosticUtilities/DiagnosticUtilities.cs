f7b0ea8ffc08efd31cc7386ed3a38e38b23835b5
Jason <dibiancoj@gmail.com>
2017-01-06 17:05:08 -0500

    spin wait timespan

---
+++ Source/ToracLibrary.Core/DiagnosticUtilities/DiagnosticUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.Core.Diagnostic
+{
+
+    /// <summary>
+    /// Random stuff used for troubleshooting stuff
+    /// </summary>
+    public static class DiagnosticUtilities
+    {
+
+        /// <summary>
+        /// Run a spin and wait until the timespan passed in has expired
+        /// </summary>
+        /// <param name="TimeSpanToSpinUntil">Time span to spin until. If you pass in 5 seconds then we will spin until that expires</param>
+        public static void SpinWaitUntilTimespan(TimeSpan TimeSpanToSpinUntil)
+        {
+            //figure out the time we need to spin until
+            DateTime SpinUntil = DateTime.Now.Add(TimeSpanToSpinUntil);
+
+            //Spin until we are past the spin until variable
+            SpinWait.SpinUntil(() => DateTime.Now > SpinUntil);
+        }
+
+    }
+
+}
---
+++ Source/ToracLibrary.UnitTest/UnitTests/Core/DiagnosticUtilities/DiagnosticUtilitiesTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToracLibrary.Core.Diagnostic;
+using Xunit;
+
+namespace ToracLibrary.UnitTest.Core
+{
+
+    /// <summary>
+    /// Unit test to Diagnostic Utilities methods
+    /// </summary>
+    public class DiagnosticUtilitiesTest
+    {
+
+        #region Months Between 2 Dates
+
+        /// <summary>
+        /// Test SpinWaitUntilTimespan
+        /// </summary>
+        [Fact]
+        public void SpinWaitUntilTimespanTest1()
+        {
+            //grab now
+            DateTime Now = DateTime.Now;
+
+            //spin until then
+            DiagnosticUtilities.SpinWaitUntilTimespan(new TimeSpan(0, 0, 3));
+
+            //grab the current time
+            var TimeNow = DateTime.Now.Subtract(Now).Seconds;
+
+            //give a little bit of a buffer to finish..so check 3 to 3.75 seconds
+            Assert.True(TimeNow >= 3 && TimeNow < 3.75);
+        }
+
+        #endregion  
+
+    }
+
+}
