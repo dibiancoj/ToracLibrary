a7ee37bf0dd1463cd2a3da05fd424ba3458501d2
Jason <dibiancoj@gmail.com>
2017-01-06 17:23:19 -0500

    date time between

---
+++ Source/ToracLibrary.Core/ExtensionMethods/DateTimeExtensions/DateTimeExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.Core.ExtensionMethods.DateTimeExtensions
+{
+
+    /// <summary>
+    /// Extension methods for date time variables
+    /// </summary>
+    public static class DateTimeExtensionMethods
+    {
+
+        /// <summary>
+        /// Evalulate if the date time is between the start and end date. Essentially ValueToEvaluate >= BeginningStartDate && ValueToEvaluate < EndStartDate
+        /// </summary>
+        /// <param name="ValueToEvaluate">Value to determine if its between the 2 time periods specified in BeginningStartDate and EndStartDate</param>
+        /// <param name="BeginningStartDate">Start date range</param>
+        /// <param name="EndStartDate">End date to range</param>
+        /// <returns>True if the ValueToEvaluate is between the specified date range</returns>
+        public static bool IsBetween(this DateTime ValueToEvaluate, DateTime BeginningStartDate, DateTime EndStartDate)
+        {
+            //make sure the start is before the end
+            if (EndStartDate < BeginningStartDate)
+            {
+                throw new ArgumentOutOfRangeException("BeginningStartDate", "Start Date Is After End Date");
+            }
+
+            //is the value between the 2 dates passed in
+            return ValueToEvaluate >= BeginningStartDate && ValueToEvaluate < EndStartDate;
+        }
+
+    }
+
+}
---
+++ Source/ToracLibrary.UnitTest/UnitTests/Core/ExtensionMethods/DateTimeExtensions/DateTimeExtensionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToracLibrary.Core.ExtensionMethods.DateTimeExtensions;
+using Xunit;
+
+namespace ToracLibrary.UnitTest.ExtensionMethods.Core
+{
+
+    /// <summary>
+    /// Unit test to Date Time Extension Methods
+    /// </summary>
+    public class DateTimeExtensionTests
+    {
+
+        /// <summary>
+        /// Unit test for DateTime.Between
+        /// </summary>
+        [Fact]
+        public void DateTimeBetweenTest1()
+        {
+            Assert.True(new DateTime(2017, 1, 2).IsBetween(new DateTime(2017, 1, 1), new DateTime(2018, 1, 1)));
+            Assert.True(new DateTime(2017, 1, 3).IsBetween(new DateTime(2017, 1, 1), new DateTime(2017, 1, 4)));
+
+            Assert.False(new DateTime(2017, 1, 3).IsBetween(new DateTime(2017, 1, 5), new DateTime(2017, 1, 6)));
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => new DateTime(2017, 1, 3).IsBetween(new DateTime(2017, 1, 2), new DateTime(2017, 1, 1)));
+        }
+
+    }
+
+}
