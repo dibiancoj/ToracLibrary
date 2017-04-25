6fc21d5a3fb464a28dfb086ff76165c6a1ac02e2
dibiancoj <dibiancoj@gmail.com>
2017-04-24 20:26:42 -0400

    buffered string reader

---
+++ Source/ToracLibrary.Core/Readers/BufferedStringReader.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.Core.Readers
+{
+
+    /// <summary>
+    /// Buffered string reader so you can peek multiple lines and still have them read back
+    /// </summary>
+    public class BufferedStringReader : IDisposable
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="StringToRead">String to iterate over</param>
+        public BufferedStringReader(string StringToRead)
+        {
+            //create a new buffer
+            Buffer = new Queue<int>();
+
+            //create a new string reader to iterate over
+            Reader = new StringReader(StringToRead);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// String reader to iterate over
+        /// </summary>
+        private readonly StringReader Reader;
+
+        /// <summary>
+        /// Buffer used so we can peek with and read back.
+        /// </summary>
+        private readonly Queue<int> Buffer;
+
+        /// <summary>
+        /// Holds a flag if the class has been disposed yet or called to be disposed yet
+        /// </summary>
+        /// <remarks>Used IDisposable</remarks>
+        private bool Disposed { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Peek at a character and don't consume it
+        /// </summary>
+        /// <param name="CharacterIndex">What index stream do you want to read. 0 would be the next character in line</param>
+        /// <returns>The character at the specified index</returns>
+        public int Peek(int CharacterIndex)
+        {
+            ///loop through the numbers of characters you want to read
+            for (int i = 0; i <= CharacterIndex; i++)
+            {
+                //do we have this in our buffer?
+                if (Buffer.Count == 0 || i >= Buffer.Count)
+                {
+                    //grab the result
+                    int ReadValue = Reader.Read();
+
+                    //if its at the end...then return
+                    if (ReadValue == -1)
+                    {
+                        //at the end. Go return that value
+                        return ReadValue;
+                    }
+
+                    //we don't have it in the buffer. Go read it and put it in the buffer
+                    Buffer.Enqueue(ReadValue);
+                }
+            }
+
+            //go grab the specified item from the buffer now
+            return Buffer.ElementAt(CharacterIndex);
+        }
+
+        /// <summary>
+        /// Read the next character and consume it
+        /// </summary>
+        /// <returns>the specified character with consume it</returns>
+        public int Read()
+        {
+            //if we have nothing in the buffer
+            if (Buffer.Count == 0)
+            {
+                //nothing in the buffer...read it from the reader and return it
+                return Reader.Read();
+            }
+
+            //we have stuff in the buffer...go grab it and return it
+            return Buffer.Dequeue();
+        }
+
+        #endregion
+
+        #region Dispose Method
+
+        /// <summary>
+        /// Disposes My Object
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Dispose Overload. Ensures my database connection is closed
+        /// </summary>
+        private void Dispose(bool disposing)
+        {
+            if (!this.Disposed)
+            {
+                if (disposing)
+                {
+                    Reader.Dispose();
+                }
+            }
+            Disposed = true;
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Source/ToracLibrary.UnitTest/UnitTests/Core/Reader/BufferedStringReaderTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using ToracLibrary.Core.Readers;
+using Xunit;
+
+namespace ToracLibrary.UnitTest.Core
+{
+
+    /// <summary>
+    /// Test for a buffered string reader
+    /// </summary>
+    public class BufferedStringReaderTest
+    {
+
+        /// <summary>
+        /// Test the buffered string reader. Simple test with multiple scenarios
+        /// </summary>
+        [Fact]
+        public void SimpleBufferedStringReaderTest1()
+        {
+            //string to test with
+            const string TestString = "test";
+
+            //no more characters
+            const string NoMoreCharacters = "-1";
+
+            //create the reader
+            using (var ReaderToUse = new BufferedStringReader(TestString))
+            {
+                //peak at the first character (should be "t")
+                Assert.Equal(TestString[0], (char)ReaderToUse.Peek(0));
+
+                //look at the next character (should be "e")
+                Assert.Equal(TestString[1], (char)ReaderToUse.Peek(1));
+
+                //now peek at the 3rd character (should be "s")
+                Assert.Equal(TestString[2], (char)ReaderToUse.Peek(2));
+
+                //go read the first character "t"
+                Assert.Equal(TestString[0], (char)ReaderToUse.Read());
+
+                //peek now...since we read t, then it should be "e"
+                Assert.Equal(TestString[1], (char)ReaderToUse.Peek(0));
+
+                //peek again after the read. Should be "s" now.
+                Assert.Equal(TestString[2], (char)ReaderToUse.Peek(1));
+
+                //now let's read and test the End of File
+                ReaderToUse.Read();
+                ReaderToUse.Read();
+                ReaderToUse.Read();
+                ReaderToUse.Read();
+
+                //make sure we return -1 on all commands
+                Assert.Equal(NoMoreCharacters, ReaderToUse.Peek(0).ToString());
+                Assert.Equal(NoMoreCharacters, ReaderToUse.Peek(0).ToString());
+
+                //test the End of file...when passing in > 0 at the end
+                Assert.Equal(NoMoreCharacters, ReaderToUse.Peek(1).ToString());
+                Assert.Equal(NoMoreCharacters, ReaderToUse.Peek(1).ToString());
+
+                //test the read method now
+                Assert.Equal(NoMoreCharacters, ReaderToUse.Read().ToString());
+                Assert.Equal(NoMoreCharacters, ReaderToUse.Read().ToString());
+            }
+        }
+
+    }
+
+}
