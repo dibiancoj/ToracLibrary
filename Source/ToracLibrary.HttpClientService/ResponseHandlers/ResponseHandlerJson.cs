72483e409b76e9b71c01ee8c2151086e439db1ae
dibiancoj <dibiancoj@gmail.com>
2017-11-06 14:33:48 -0500

    remove using

--- Source/ToracLibrary.HttpClientService/ResponseHandlers/ResponseHandlerJson.cs
+++ Source/ToracLibrary.HttpClientService/ResponseHandlers/ResponseHandlerJson.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-using System.IO;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using ToracLibrary.HttpClientService.RequestBuilder;
 using ToracLibrary.Serialization.Json;
