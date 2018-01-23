6984cb76cb20c259ff19abd9e1e8d5a81b97a6e1
Jason <dibiancoj@gmail.com>
2018-01-22 19:37:20 -0500

    webpack standard config

---
+++ Docs/Examples/WebPack/StandardConfig/webpack.config.js
@@ -0,0 +1,73 @@
+﻿"use strict";
+
+/*
+  "devDependencies": {
+    "webpack": "3.10.0",
+    "babel-core": "6.26.0",
+    "babel-loader": "7.1.2",
+    "babel-preset-es2015": "6.24.1",
+    "clean-webpack-plugin": "0.1.17",
+    "path": "0.12.7"
+  }
+*/
+
+//"webpack -p --env.production" --> for production build (you are basically setting the env variable below)
+
+//*** note this example uses env.production vs setting the node env variable.
+//to use the alternative "SET NODE_ENV=production&& webpack -p"...then in config below use console.log('Node Env = ', process.env.NODE_ENV);. I think using the web pack variables is better for now
+//i like to use p because it minifies my code without pulling in more plugins
+
+//webpack 
+//webpack --env.production = true - p
+
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = env => {
+
+    const isProduction = env && env.production ? true : false;
+    console.log('Production: ', isProduction);
+
+    return {
+
+        entry: {
+            Entry1: './App/MyEntryPoint1.js',
+            Entry2: './App/MyEntryPoint2.js',
+        },
+
+        output: {
+            filename: './wwwroot/Build/App/[name].Bundle.js'
+        },
+
+        devtool: isProduction ? 'none' : 'source-map',
+
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader?presets[]=es2015'
+                }
+            ]
+        },
+
+        plugins:
+        [
+          //this just cleans up the output folder so we don't have left over items that shouldn't be there.
+            new CleanWebpackPlugin([path.join(__dirname, '/wwwroot/Build/App/*.*')]), //removes all files in this directory. Or you can do '/wwwroot/Build/**.js' if you want to delete just the js files
+
+            //if i don't want to import jquery on every screen i can do this and this will make jquery global
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jquery: "jquery",
+                "window.jQuery": "jquery",
+                jQuery: "jquery"
+            }),
+
+          new webpack.DllReferencePlugin({
+                context: '.',
+                manifest: require('./wwwroot/Build/Framework/vendor-manifest.json'),
+            })
+        ]
+    };
+};
---
+++ Docs/Examples/WebPack/StandardConfig/webpack.vendor.config.js
@@ -0,0 +1,67 @@
+﻿//webpack --config webpack.vendor.config.js
+//webpack --config webpack.vendor.config.js--env.production = true - p
+
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = env => {
+
+    const isProduction = env && env.production ? true : false;
+    console.log('Production: ', isProduction);
+
+    const extractCSS = new ExtractTextPlugin('./wwwroot/Build/Framework/vendor.css');
+
+    return {
+
+        entry: {
+            Vendor: ['jquery', 'bootstrap', 'bootstrap/dist/css/bootstrap.css'],
+        },
+        output: {
+            filename: './wwwroot/Build/Framework/vendor.bundle.js',
+            library: 'vendor_lib',
+        },
+        module: {
+            loaders: [
+                //css for bootstrap (or any css) import
+                {
+                    test: /\.css(\?|$)/,
+                    use: extractCSS.extract({ use: !isProduction ? 'css-loader' : 'css-loader?minimize' })
+                },
+                {
+                    test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './wwwroot/Build/Framework/Fonts/'    // where the fonts will go
+                        }
+                    }]
+                },
+            ]
+        },
+        plugins: [
+
+            extractCSS, 
+
+            //this just cleans up the output folder so we don't have left over items that shouldn't be there.
+            new CleanWebpackPlugin([path.join(__dirname, '/wwwroot/Build/Framework/*.*')]), //removes all files in this directory. Or you can do '/wwwroot/Build/**.js' if you want to delete just the js files
+
+            new webpack.DllPlugin({
+                name: 'vendor_lib',
+                path: './wwwroot/Build/Framework/vendor-manifest.json',
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+
+                //with out any additional parameters everything gets ordered from right to left. So vendor 2 has all the web pack plumbing. Then vendor just has its own stuff.
+                name: ["Vendor"],
+
+                //*** if you get a jsonp error with entry point it means the entry point is being loaded before the common chunk (vendor)
+                //(with more entries, this ensures that no other module
+                //goes into the vendor chunk)
+                minChunks: Infinity
+            })
+        ]
+    }
+};
