ade1c8d1f27f70fe7c5363e30d3a99c15466486c
Jason DiBianco <dibiancj@mskcc.org>
2018-03-15 13:53:24 -0400

    Bad request result...and auto validate model attribute

---
+++ Source/ToracLibrary.AspNet.AspNetMVC/CustomActionsResults/BadRequestResult.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ToracLibrary.AspNet.AspNetMVC.CustomActionsResults
+{
+
+    /// <summary>
+    /// Used to validation errors with ajax calls. Will return a 400 error. Works like web api.
+    /// </summary>
+    public class BadRequestResult : JsonResult
+    {
+        //client side
+        //--to get data from above call use jqxhr.responseText
+        //$(document).ajaxError(function (event, jqxhr, settings, thrownError) {
+        //            if (jqxhr.status == 400)
+        //            {
+        //                  $('#errorFound').text(JSON.parse(jqxhr.responseJSON)); //or use jqxhr.responseText
+        //              $('#errorBanner').show();
+        //            }
+        //        });
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="ModelState">Model state</param>
+        public BadRequestResult(ModelStateDictionary ModelState)
+        {
+            this.Data = ModelState.Values.SelectMany(x => x.Errors);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Execute the results
+        /// </summary>
+        /// <param name="Context">Context</param>
+        public override void ExecuteResult(ControllerContext Context)
+        {
+            //set the response status code to 400
+            Context.RequestContext.HttpContext.Response.StatusCode = 400;
+
+            //go run the base
+            base.ExecuteResult(Context);
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Source/ToracLibrary.AspNet.AspNetMVC/CustomFilters/AutoValidateModelAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ToracLibrary.AspNet.AspNetMVC.CustomActionsResults;
+
+namespace ToracLibrary.AspNet.AspNetMVC.CustomFilters
+{
+
+    /// <summary>
+    /// Add this attribute to your controller method for ajax calls and it will return a 400 bad request if the modelstate isn't valid
+    /// </summary>
+    public class AutoValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.Controller.ViewData.ModelState.IsValid)
+            {
+                context.Result = new BadRequestResult(context.Controller.ViewData.ModelState);
+            }
+        }
+    }
+}
