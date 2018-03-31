29f2f080a7af050512a973c70c160b6571fc41a2
Jason <dibiancoj@gmail.com>
2018-03-30 22:30:25 -0400

    init right of the new object

--- Source/ToracLibrary.Core.EntityFrameworkDP/EFUnTypedDP.cs
+++ Source/ToracLibrary.Core.EntityFrameworkDP/EFUnTypedDP.cs
@@ -25,29 +25,29 @@ namespace ToracLibrary.Core.DataProviders.EntityFrameworkDP
         /// <returns>connection string to use</returns>
         public static string BuildConnectionString(string ServerName, string DatabaseName)
         {
-            // Initialize the connection string builder for the
-            // underlying provider.
-            var Builder = new SqlConnectionStringBuilder();
-
-            // Set the properties for the data source.
-            Builder.DataSource = ServerName;
-            Builder.InitialCatalog = DatabaseName;
-            Builder.IntegratedSecurity = true;
+            //Initialize the connection string builder for the
+            //underlying provider.
+            var Builder = new SqlConnectionStringBuilder
+            {
+                //Set the properties for the data source.
+                DataSource = ServerName,
+                InitialCatalog = DatabaseName,
+                IntegratedSecurity = true
+            };
 
             // Initialize the EntityConnectionStringBuilder.
-            var EntityStringBuilder = new EntityConnectionStringBuilder();
+            return new EntityConnectionStringBuilder
+            {
+                //Set the provider name.
+                Provider = "System.Data.SqlClient",
 
-            //Set the provider name.
-            EntityStringBuilder.Provider = "System.Data.SqlClient";
+                //Set the provider-specific connection string.
+                ProviderConnectionString = Builder.ToString(),
 
-            // Set the provider-specific connection string.
-            EntityStringBuilder.ProviderConnectionString = Builder.ToString();
+                //Set the Metadata location.
+                Metadata = "res://*/",
 
-            // Set the Metadata location.
-            EntityStringBuilder.Metadata = "res://*/";
-
-            //return the string now
-            return EntityStringBuilder.ToString();
+            }.ToString();
         }
 
         #endregion
