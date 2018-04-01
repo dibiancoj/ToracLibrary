ef49769f0936e646e7019877b64baab68875f966
Jason <dibiancoj@gmail.com>
2018-03-31 21:25:54 -0400

    cleanup using

--- Source/ToracLibrary.Core.EntityFrameworkDP/EntityFrameworkDP.cs
+++ Source/ToracLibrary.Core.EntityFrameworkDP/EntityFrameworkDP.cs
@@ -7,7 +7,6 @@ using System.Data.Entity.Migrations;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 using System.Transactions;
 using ToracLibrary.Core.DataProviders.ADO;
