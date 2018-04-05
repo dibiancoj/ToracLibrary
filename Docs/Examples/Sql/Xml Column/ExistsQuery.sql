c8a8d43be5e426ef66644ddf5f67bc0ac631a6d3
Jason DiBianco <dibiancoj@gmail.com>
2018-04-05 13:53:02 -0400

    Update ExistsQuery.sql

--- Docs/Examples/Sql/Xml Column/ExistsQuery.sql
+++ Docs/Examples/Sql/Xml Column/ExistsQuery.sql
@@ -1,3 +1,11 @@
 ﻿select *
 from Ref_SavedSearch as s
 where s.GridSettings.exist('//GridColumn[@Name="Branch"]') = 'True'
+
+
+//<Event_ID>123</Event_ID>
+select *
+from CohortDataHistory as cd
+where cd.ID = 1168 
+and cd.Data.exist('//EVENT_ID[.=123]') = 'True'
+order by cd.TriggeredOn desc
