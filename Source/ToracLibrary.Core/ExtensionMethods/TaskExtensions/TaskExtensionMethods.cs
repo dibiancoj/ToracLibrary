e22f7a8b2eb28266cedf56c535bdedee23b0c65f
Jason DiBianco <dibiancoj@gmail.com>
2018-10-23 14:19:12 -0400

    task then

---
+++ Source/ToracLibrary.Core/ExtensionMethods/TaskExtensions/TaskExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToracLibrary.Core.ExtensionMethods.TaskExtensions
+{
+
+    /// <summary>
+    /// Task based extension methods
+    /// </summary>
+    public static class TaskExtensionMethods
+    {
+
+        /// <summary>
+        /// Use a continuation based flow to run code after a task returns successfully
+        /// </summary>
+        /// <typeparam name="TTaskResult">Result type of the task</typeparam>
+        /// <typeparam name="TMethodResult">Result of the method after the continuation</typeparam>
+        /// <param name="Antecedent">Task to await</param>
+        /// <param name="Continuation">continuation code to run and return the result of</param>
+        /// <returns></returns>
+        public static async Task<TMethodResult> Then<TTaskResult, TMethodResult>(this Task<TTaskResult> Antecedent, Func<TTaskResult, TMethodResult> Continuation)
+        {
+            //run the continuation and return the result
+            return Continuation(await Antecedent);
+        }
+
+    }
+
+}
---
+++ Source/ToracLibrary.UnitTest/UnitTests/Core/ExtensionMethods/TaskExtensions/TaskExtensionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToracLibrary.Core.ExtensionMethods.TaskExtensions;
+using Xunit;
+
+namespace ToracLibrary.UnitTest.ExtensionMethods.Core
+{
+
+    public class TaskExtensionTest
+    {
+
+        private static async Task<string> AsyncStubMethod()
+        {
+            await Task.Delay(50);
+
+            return "Test 123";
+        }
+
+        [Fact]
+        public async Task ThenResultTest()
+        {
+            Assert.Equal("T", await AsyncStubMethod().Then(tsk => tsk.Substring(0, 1)));
+        }
+
+    }
+
+}
